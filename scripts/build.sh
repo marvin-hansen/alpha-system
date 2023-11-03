12b47f4d7b582e253ffe8fbcee6a880a0c5827d4
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-03 15:24:55 +0800

    Added scripts for make
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) "2023" . The DeepCausality Authors. All Rights Reserved.
+
+# bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+command cargo build
---
+++ scripts/check.sh
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) "2023" . The DeepCausality Authors. All Rights Reserved.
+
+# bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Check for outdated dependencies
+# https://github.com/kbknapp/cargo-outdated
+command cargo outdated --workspace
+
+
+# Scan for unused dependencies
+# https://crates.io/crates/cargo-udeps
+command cargo +nightly udeps --all-targets
+
+
+# Scan again to report all unfixed vulnerabilities
+# https://crates.io/crates/cargo-audit
+command cargo audit
+
+
+# Check a package and all of its dependencies for errors.
+# https://doc.rust-lang.org/cargo/commands/cargo-check.html
+command cargo check --all-targets
+
+# Consider checking each crate for re-exporting external types
+# https://crates.io/crates/cargo-check-external-types
+# cargo +nightly check-external-types
+
+
+# Check for linter errors
+# https://github.com/rust-lang/rust-clippy
+command cargo clippy --all-targets
+
+
+# Check code formatting
+# https://github.com/rust-lang/rustfmt
+command cargo fmt --all --check
