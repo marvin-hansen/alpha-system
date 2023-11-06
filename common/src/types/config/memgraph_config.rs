ceb9daaaa8ab23ab1062120b3095012774799df6
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-06 10:56:51 +0800

    Updated MemGraphConfig and tests
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- common/src/types/config/memgraph_config.rs
+++ common/src/types/config/memgraph_config.rs
@@ -1,10 +1,10 @@
 use std::fmt::{Debug, Display, Formatter};
 
-use rsmgclient::SSLMode;
+use rsmgclient::{ConnectParams, SSLMode};
 
-pub type CallbackType = Option<*const dyn Fn(&String, &String, &String, &String) -> i32>;
+const CLIENT_NAME: &str = "rsmgclient/2.0.2";
 
-#[derive(Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct MemGraphConfig {
     /// Port number to connect to at the server host. Default port is 7687.
     port: u16,
@@ -22,39 +22,26 @@ pub struct MemGraphConfig {
     /// Alternate name and version of the client to send to server. Default is
     /// "MemgraphBolt/0.1".
     client_name: String,
-    /// Determines whether a secure SSL TCP/IP connection will be negotiated with the server.
-    /// Default value is `SSLMode::Require`.
-    sslmode: SSLMode,
-    /// This parameter specifies the file name of the client SSL certificate. It is ignored in
-    /// case an SSL connection is not made.
-    sslcert: Option<String>,
-    /// This parameter specifies the location of the secret key used for the client certificate.
-    /// This parameter is ignored in case an SSL connection is not made.
-    sslkey: Option<String>,
-    /// After performing the SSL handshake, `Connection::connect` will call this function providing
-    /// the hostname, IP address, public key type and fingerprint and user provided data. If the
-    /// function returns a non-zero value, SSL connection will be immediately terminated. This can
-    /// be used to implement TOFU (trust on first use) mechanism.
-    trust_callback: CallbackType,
-    /// Initial value of `lazy` field, defaults to true, Can be changed using `Connection::set_lazy`.
-    lazy: bool,
-    /// Initial value of `autocommit` field, defaults to false. Can be changed using
-    /// `Connection::set_autocommit`.
-    autocommit: bool,
 }
 
-// constructors
 impl MemGraphConfig {
-    pub fn new_unsecure(port: u16, host: Option<String>) -> Self {
+    pub fn new_connection(port: u16, host: Option<String>) -> Self {
         Self {
             port,
             host,
-            sslmode: SSLMode::Disable,
             ..Default::default()
         }
     }
 
-    pub fn new_with_authentication(
+    pub fn new_authentication(username: Option<String>, password: Option<String>) -> Self {
+        Self {
+            username,
+            password,
+            ..Default::default()
+        }
+    }
+
+    pub fn new_connection_with_authentication(
         port: u16,
         host: Option<String>,
         username: Option<String>,
@@ -65,96 +52,20 @@ impl MemGraphConfig {
             host,
             username,
             password,
-            sslmode: SSLMode::Disable,
             ..Default::default()
         }
     }
-}
 
-impl Default for MemGraphConfig {
-    fn default() -> Self {
-        Self {
-            port: 7687,
-            host: None,
-            address: None,
-            username: None,
-            password: None,
-            client_name: String::from("rsmgclient/2.0.2"),
+    pub fn get_connect_params(&self) -> ConnectParams {
+        ConnectParams {
+            host: self.host.clone(),
+            port: self.port,
             sslmode: SSLMode::Disable,
-            sslcert: None,
-            sslkey: None,
-            trust_callback: None,
-            lazy: true,
-            autocommit: false,
+            username: self.username.clone(),
+            password: self.password.clone(),
+            client_name: self.client_name.clone(),
+            ..Default::default()
         }
-    }
-}
-
-impl MemGraphConfig {
-    pub fn convert_to_string(&self) -> String {
-        let mut s = String::new();
-
-        s.push_str(&self.client_name);
-
-        s.push_str(&format!(" {}", self.port));
-
-        if let Some(ref host) = self.host {
-            s.push_str(&format!(" {}", host));
-        }
-        if let Some(ref address) = self.address {
-            s.push_str(&format!(" {}", address));
-        }
-        if let Some(ref username) = self.username {
-            s.push_str(&format!(" {}", username));
-        }
-        if let Some(ref password) = self.password {
-            s.push_str(&format!(" {}", password));
-        }
-
-        s.push_str(&format!(" {}", ssl_mode_to_string(&self.sslmode)));
-
-        if let Some(ref sslcert) = self.sslcert {
-            s.push_str(&format!(" {}", sslcert));
-        }
-
-        s.push_str(&format!(" {}", &self.lazy));
-
-        s.push_str(&format!(" {}", &self.autocommit));
-
-        s
-    }
-}
-
-fn ssl_mode_to_string(sslmode: &SSLMode) -> String {
-    match sslmode {
-        SSLMode::Disable => String::from("Disable"),
-        SSLMode::Require => String::from("Require"),
-    }
-}
-
-impl Debug for MemGraphConfig {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("MemGraphConfig")
-            .field("port", &self.port)
-            .field("host", &self.host)
-            .field("address", &self.address)
-            .field("username", &self.username)
-            .field("password", &self.password)
-            .field("client_name", &self.client_name)
-            // `SSLMode` cannot be formatted using `{:?}` because it doesn't implement `Debug`
-            // .field("sslmode", ssl_mode_to_string(self.sslmode))
-            .field("sslcert", &self.sslcert)
-            .field("sslkey", &self.sslkey)
-            .field("trust_callback", &self.trust_callback)
-            .field("lazy", &self.lazy)
-            .field("autocommit", &self.autocommit)
-            .finish()
-    }
-}
-
-impl Display for MemGraphConfig {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        std::fmt::Display::fmt(&self.convert_to_string(), f)
     }
 }
 
@@ -178,62 +89,23 @@ impl MemGraphConfig {
     pub fn client_name(&self) -> &str {
         &self.client_name
     }
-    pub fn sslmode(&self) -> &SSLMode {
-        &self.sslmode
-    }
-    pub fn sslcert(&self) -> &Option<String> {
-        &self.sslcert
-    }
-    pub fn sslkey(&self) -> &Option<String> {
-        &self.sslkey
-    }
-    pub fn trust_callback(&self) -> CallbackType {
-        self.trust_callback
-    }
-    pub fn lazy(&self) -> bool {
-        self.lazy
-    }
-    pub fn autocommit(&self) -> bool {
-        self.autocommit
+}
+
+impl Default for MemGraphConfig {
+    fn default() -> Self {
+        Self {
+            port: 7687,
+            host: None,
+            address: None,
+            username: None,
+            password: None,
+            client_name: String::from(CLIENT_NAME),
+        }
     }
 }
 
-// setters
-impl MemGraphConfig {
-    pub fn set_port(&mut self, port: u16) {
-        self.port = port;
-    }
-    pub fn set_host(&mut self, host: Option<String>) {
-        self.host = host;
-    }
-    pub fn set_address(&mut self, address: Option<String>) {
-        self.address = address;
-    }
-    pub fn set_username(&mut self, username: Option<String>) {
-        self.username = username;
-    }
-    pub fn set_password(&mut self, password: Option<String>) {
-        self.password = password;
-    }
-    pub fn set_client_name(&mut self, client_name: String) {
-        self.client_name = client_name;
-    }
-    pub fn set_sslmode(&mut self, sslmode: SSLMode) {
-        self.sslmode = sslmode;
-    }
-    pub fn set_sslcert(&mut self, sslcert: Option<String>) {
-        self.sslcert = sslcert;
-    }
-    pub fn set_sslkey(&mut self, sslkey: Option<String>) {
-        self.sslkey = sslkey;
-    }
-    pub fn set_trust_callback(&mut self, trust_callback: CallbackType) {
-        self.trust_callback = trust_callback;
-    }
-    pub fn set_lazy(&mut self, lazy: bool) {
-        self.lazy = lazy;
-    }
-    pub fn set_autocommit(&mut self, autocommit: bool) {
-        self.autocommit = autocommit;
+impl Display for MemGraphConfig {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self, f)
     }
 }
--- common/tests/types/config/memgraph_config.rs
+++ common/tests/types/config/memgraph_config.rs
@@ -1,16 +1,16 @@
 use common::types::config::memgraph_config::MemGraphConfig;
 
 #[test]
-fn test_new_unsecure() {
-    let config = MemGraphConfig::new_unsecure(7687, Some("localhost".to_string()));
+fn test_new_connection() {
+    let config = MemGraphConfig::new_connection(7687, Some("localhost".to_string()));
 
     assert_eq!(config.port(), 7687);
     assert_eq!(config.host(), &Some("localhost".to_string()));
 }
 
 #[test]
-fn test_new_with_authentication() {
-    let config = MemGraphConfig::new_with_authentication(
+fn test_new_connection_with_authentication() {
+    let config = MemGraphConfig::new_connection_with_authentication(
         7687,
         Some("localhost".to_string()),
         Some("username".to_string()),
@@ -24,6 +24,41 @@ fn test_new_with_authentication() {
 }
 
 #[test]
+fn test_new_authentication() {
+    let config = MemGraphConfig::new_authentication(
+        Some("username".to_string()),
+        Some("password".to_string()),
+    );
+
+    assert_eq!(config.username(), &Some("username".to_string()));
+    assert_eq!(config.password(), &Some("password".to_string()));
+}
+
+#[test]
+fn test_get_connect_params() {
+    let config = MemGraphConfig::new_connection_with_authentication(
+        7687,
+        Some("localhost".to_string()),
+        Some("username".to_string()),
+        Some("password".to_string()),
+    );
+
+    assert_eq!(config.port(), 7687);
+    assert_eq!(config.host(), &Some("localhost".to_string()));
+    assert_eq!(config.username(), &Some("username".to_string()));
+    assert_eq!(config.password(), &Some("password".to_string()));
+    assert_eq!(config.client_name(), "rsmgclient/2.0.2");
+
+    let connect_params = config.get_connect_params();
+
+    assert_eq!(connect_params.host, Some("localhost".to_string()));
+    assert_eq!(connect_params.port, 7687);
+    assert_eq!(connect_params.username, Some("username".to_string()));
+    assert_eq!(connect_params.password, Some("password".to_string()));
+    assert_eq!(connect_params.client_name, "rsmgclient/2.0.2");
+}
+
+#[test]
 fn test_default() {
     let d = MemGraphConfig::default();
 
@@ -33,11 +68,4 @@ fn test_default() {
     assert_eq!(d.username(), &None);
     assert_eq!(d.password(), &None);
     assert_eq!(d.client_name(), "rsmgclient/2.0.2");
-    // `SSLMode` cannot be formatted using `{:?}` because it doesn't implement `Debug`
-    // assert_eq!(d.sslmode(), &SSLMode::Disable);
-    assert_eq!(d.sslcert(), &None);
-    assert_eq!(d.sslkey(), &None);
-    // assert_eq!(d.trust_callback(), None);
-    assert!(d.lazy());
-    assert!(!d.autocommit());
 }
