862f05f6a9420ab22ff65eec5e680c54c222c5e1
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-06 22:06:05 +0800

    Added mock implementation of DnsManager.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- components/tests/ctx_manager_tests/ctx_manager_tests.rs
+++ components/tests/ctx_manager_tests/ctx_manager_tests.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use common::prelude::EnvironmentType;
 use components::prelude::CtxManager;
 
@@ -6,6 +8,33 @@ fn test_new() {
     let ctm = CtxManager::new();
     assert_eq!(ctm.env_type(), EnvironmentType::UnknownEnv);
     assert_eq!(ctm.int_dns_server(), &None);
+}
+
+
+#[test]
+fn test_new_local() {
+    let key = "ENV";
+    let value = "LOCAL";
+    env::set_var(key, value);
+
+    let ctm = CtxManager::new();
+    assert_eq!(ctm.env_type(), EnvironmentType::LOCAL);
+    assert_eq!(ctm.int_dns_server(), &None);
+}
+
+#[test]
+fn test_new_cluster() {
+    let key = "ENV";
+    let value = "CLUSTER";
+    env::set_var(key, value);
+
+    let key = "DNS_SERVER";
+    let value = "175.24.54.1";
+    env::set_var(key, value);
+
+    let ctm = CtxManager::new();
+    assert_eq!(ctm.env_type(), EnvironmentType::CLUSTER);
+    assert_eq!(ctm.int_dns_server(), &Some("175.24.54.1".to_string()));
 }
 
 #[test]
