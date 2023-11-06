8d6ac3c927ad8d5d49a81d34c31f7c55800efd21
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-06 18:06:53 +0800

    Working on DNS manager.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- components/src/dns_manager/mod.rs
+++ components/src/dns_manager/mod.rs
@@ -1,1 +1,16 @@
+use std::fmt::Error;
 
+use common::prelude::{DnsConfig, EnvironmentType};
+
+use crate::dns_manager::dns_manager_impl::{DnsManagerImpl, Traited};
+
+pub mod dns_manager_impl;
+
+pub fn new_dns_manager(dns_config: DnsConfig, env_type: EnvironmentType) -> Result<Traited, Error> {
+    Ok(Traited::from(Traited::DnsManagerImpl(DnsManagerImpl::new(dns_config, env_type))))
+}
+
+pub trait DnsManager {
+    fn resolve_cluster_dns(&self, hostname: &str) -> Result<String, Error>;
+    fn resolve_remote_dns(&self, hostname: &str) -> Result<String, Error>;
+}
