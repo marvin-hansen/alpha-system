891f7d81055029c4d179a20dff143bb1147767e3
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-07 14:39:24 +0800

    Documented config types.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- common/src/types/config_types/main_config.rs
+++ common/src/types/config_types/main_config.rs
@@ -2,6 +2,14 @@ use std::fmt::{Display, Formatter};
 
 use crate::prelude::{ProtocolType, ServiceID};
 
+/// MainConfig represents the main configuration for the service.
+///
+/// # Fields
+///
+/// * `id`: The service ID.
+/// * `name`: The service name.
+/// * `port`: The port number.
+/// * `protocol`: The protocol type.
 #[derive(Debug, Default, Clone, Eq, PartialEq)]
 pub struct MainConfig {
     id: ServiceID,
@@ -11,6 +19,7 @@ pub struct MainConfig {
 }
 
 impl MainConfig {
+    /// Creates a new MainConfig with the given fields.
     pub fn new(id: ServiceID, name: String, port: u16, protocol: ProtocolType) -> Self {
         Self {
             id,
@@ -19,18 +28,23 @@ impl MainConfig {
             protocol,
         }
     }
-}
 
-impl MainConfig {
+    /// Returns the service ID.
     pub fn id(&self) -> &ServiceID {
         &self.id
     }
+
+    /// Returns the service name.
     pub fn name(&self) -> &str {
         &self.name
     }
+
+    /// Returns the port number.
     pub fn port(&self) -> u16 {
         self.port
     }
+
+    /// Returns the protocol type.
     pub fn protocol(&self) -> &ProtocolType {
         &self.protocol
     }
--- common/src/types/config_types/service_id.rs
+++ common/src/types/config_types/service_id.rs
@@ -1,5 +1,12 @@
 use std::fmt::{Display, Formatter};
 
+/// An Enum that represents the unique ID of a service.
+///
+/// # Variants
+///
+/// * `MEMGRAPH`: The Memgraph service.
+/// * `SMDB`: The SMDb service.
+/// * `CMDB`: The CMDB service.
 #[derive(Debug, Default, Copy, Clone, Eq, PartialEq)]
 pub enum ServiceID {
     MEMGRAPH,
