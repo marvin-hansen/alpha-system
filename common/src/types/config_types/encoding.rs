62ff94e77081b32ca749ac8b942265e62f00b24d
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-07 16:42:41 +0800

    Updated config files
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- common/src/types/config_types/encoding.rs
+++ common/src/types/config_types/encoding.rs
@@ -1,6 +1,6 @@
 use std::fmt::{Display, Formatter};
 
-/// An Enum representing the encoding format used for a message.
+/// An Enum representing the encoding format used for network communication.
 ///
 /// # Variants
 ///
@@ -8,6 +8,7 @@ use std::fmt::{Display, Formatter};
 /// * `SBE`: The SBE (Simple Binary Encoding) format.
 #[derive(Debug, Copy, Clone, Default, Eq, PartialEq)]
 pub enum Encoding {
+    Binary,
     #[default]
     Protobuf,
     SBE,
@@ -16,6 +17,7 @@ pub enum Encoding {
 impl Display for Encoding {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
+            Encoding::Binary => write!(f, "Binary"),
             Encoding::Protobuf => write!(f, "Protobuf"),
             Encoding::SBE => write!(f, "SBE"),
         }
