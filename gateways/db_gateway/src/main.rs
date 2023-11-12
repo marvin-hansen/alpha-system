83b5a0aa598c22a8cc4937a56dd6f4dcb243336c
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-12 19:52:19 +0800

    Added minimal db gateway to test surreal db
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ gateways/db_gateway/src/main.rs
@@ -0,0 +1,24 @@
+use surrealdb::engine::local::{Db, Mem};
+use surrealdb::Surreal;
+
+pub mod db_manager;
+mod types;
+
+
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
+    // Connect to the server
+    let db: Surreal<Db> = Surreal::new::<Mem>(()).await?;
+
+    // Select a specific namespace / database
+    db.use_ns("test").use_db("test").await?;
+
+    // Build a new db manager
+    let dbm = db_manager::DBManager::new(db);
+
+    let res = dbm.get_record().await?;
+
+    assert!(res.is_none());
+
+    Ok(())
+}
