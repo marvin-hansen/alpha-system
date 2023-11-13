041b138bc978e82e1e1ecea1eae609fd434164a8
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-13 16:02:54 +0800

    Added DBManager to DBGateway
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- gateways/db_gateway/src/main.rs
+++ gateways/db_gateway/src/main.rs
@@ -1,20 +1,26 @@
-use surrealdb::engine::local::{Db, Mem};
+use surrealdb::engine::local;
+use surrealdb::Error;
 use surrealdb::Surreal;
+
+use components::prelude::{DBManager, Status};
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
-    // Connect to the server
-    let db: Surreal<Db> = Surreal::new::<Mem>(()).await?;
-
-    // Select a specific namespace / database
-    db.use_ns("test").use_db("test").await?;
 
     // Build a new db manager
-    // let dbm = db_manager::DBManager::new(db);
-    //
-    // let res = dbm.get_record().await?;
-    //
-    // assert!(res.is_none());
+    let dbm = get_dbm().await.unwrap();
+
+    let status = dbm.status().await.unwrap();
+    assert_eq!(status, Status::Connected);
+
 
     Ok(())
+}
+
+async fn get_dbm() -> Result<DBManager, Error> {
+    let db: Surreal<local::Db> = Surreal::new::<local::Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+    let dbm = DBManager::new(db);
+
+    Ok(dbm)
 }
