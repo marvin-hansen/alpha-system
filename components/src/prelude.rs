49ea3573dea32f81ce67b14dd4aa9f4fdb39966b
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-14 18:41:16 +0800

    Added preliminary rpc implementation for dbgateway service.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- components/src/prelude.rs
+++ components/src/prelude.rs
@@ -6,6 +6,5 @@ pub use crate::common::env_manager::*;
 pub use crate::common::svc_manager::*;
 // data
 pub use crate::data::db_manager::DBManager;
-pub use crate::data::db_manager::Status;
 pub use crate::data::smdb_data_manager::*;
 
