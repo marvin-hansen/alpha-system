78e3fef94fc52a2dfed754ede61e4f8a28f937e9
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-16 08:47:32 +0800

    Implemented SMDB client.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ clients/smdb_client/src/lib.rs
@@ -0,0 +1,33 @@
+use std::net::IpAddr;
+use std::str::FromStr;
+
+use tarpc::client;
+use tarpc::tokio_serde::formats::Bincode;
+
+use common::prelude::HostEndpoint;
+use service::service::SMDBServiceClient;
+
+mod svc_smdb;
+
+pub struct SMDBClient {
+    client: SMDBServiceClient,
+}
+
+impl SMDBClient {
+    pub async fn new(config: HostEndpoint<'_>) -> Self {
+        let port = config.port();
+        let host = config.host_uri();
+
+        let ip_addr = IpAddr::from_str(host).expect("Failed to parse IP address from DBConfig");
+        let server_addr = ((ip_addr), port);
+        let codec_fn = Bincode::default;
+
+        let mut transport = tarpc::serde_transport::tcp::connect(server_addr, codec_fn);
+        transport.config_mut().max_frame_length(usize::MAX);
+
+        let client =
+            SMDBServiceClient::new(client::Config::default(), transport.await.unwrap()).spawn();
+
+        Self { client }
+    }
+}
---
+++ clients/smdb_client/src/svc_smdb.rs
@@ -0,0 +1,55 @@
+use tarpc::context;
+
+use common::prelude::{SMDBError, ServiceID};
+
+use crate::SMDBClient;
+
+impl SMDBClient {
+    async fn check_if_service_id_exists(self, id: ServiceID) -> Result<bool, SMDBError> {
+        let res = self
+            .client
+            .check_if_service_id_exists(context::current(), id)
+            .await
+            .expect("RPC call failed to check if service id exists");
+        match res {
+            Ok(res) => Ok(res),
+            Err(e) => Err(SMDBError(e.to_string())),
+        }
+    }
+
+    async fn check_if_services_exists(self, services: Vec<ServiceID>) -> Result<bool, SMDBError> {
+        let res = self
+            .client
+            .check_if_services_exists(context::current(), services)
+            .await
+            .expect("RPC call failed to check if services exists");
+        match res {
+            Ok(res) => Ok(res),
+            Err(e) => Err(SMDBError(e.to_string())),
+        }
+    }
+
+    async fn set_service_online(self, id: ServiceID) -> Result<bool, SMDBError> {
+        let res = self
+            .client
+            .set_service_online(context::current(), id)
+            .await
+            .expect("RPC call failed to set service online");
+        match res {
+            Ok(res) => Ok(res),
+            Err(e) => Err(SMDBError(e.to_string())),
+        }
+    }
+
+    async fn set_service_offline(self, id: ServiceID) -> Result<bool, SMDBError> {
+        let res = self
+            .client
+            .set_service_offline(context::current(), id)
+            .await
+            .expect("RPC call failed to set service online");
+        match res {
+            Ok(res) => Ok(res),
+            Err(e) => Err(SMDBError(e.to_string())),
+        }
+    }
+}
