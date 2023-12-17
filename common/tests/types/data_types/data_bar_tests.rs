d25167143fccd6c15a625ec32234da6e7a90c36e
Marvin Hansen <marvin.hansen@gmail.com>
2023-12-17 17:11:14 +0800

    Fixed broken tests
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- common/tests/types/data_types/data_bar_tests.rs
+++ common/tests/types/data_types/data_bar_tests.rs
@@ -4,41 +4,40 @@ use rust_decimal::Decimal;
 #[test]
 fn test_data_bar_new() {
     let bar = DataBar::new(
+        "2020-01-01T00:00:00".to_string(),
         "AAPL".to_string(),
-        "1Min".to_string(),
-        "2022-01-01".to_string(),
         Decimal::from(100),
         Decimal::from(110),
         Decimal::from(90),
         Decimal::from(105),
         Decimal::from(1000),
+        Decimal::from(500),
     );
 
+    assert_eq!(bar.date_time(), "2020-01-01T00:00:00");
     assert_eq!(bar.symbol(), "AAPL");
-    assert_eq!(bar.timeframe(), "1Min");
-    assert_eq!(bar.period(), "2022-01-01");
     assert_eq!(bar.open(), Decimal::from(100));
     assert_eq!(bar.high(), Decimal::from(110));
     assert_eq!(bar.low(), Decimal::from(90));
     assert_eq!(bar.close(), Decimal::from(105));
     assert_eq!(bar.volume(), Decimal::from(1000));
+    assert_eq!(bar.trades(), Decimal::from(500));
 }
 
 #[test]
 fn test_data_bar_display() {
-    let bar = DataBar::new(
+    let actual = DataBar::new(
+        "2020-01-01T00:00:00".to_string(),
         "AAPL".to_string(),
-        "1Min".to_string(),
-        "2022-01-01".to_string(),
         Decimal::from(100),
         Decimal::from(110),
         Decimal::from(90),
         Decimal::from(105),
         Decimal::from(1000),
+        Decimal::from(500),
     );
 
-    assert_eq!(
-        format!("{}", bar),
-        "DataBar [symbol: AAPL, timeframe: 1Min, period: 2022-01-01, open: 100, high: 110, low: 90, close: 105, volume: 1000]"
-    );
+    let expected = "DataTime: 2020-01-01T00:00:00,\n Symbol AAPL,\n Open 100,\n High 110,\n Low 90,\n Close 105,\n Volume 1000,\n Trades 500";
+
+    assert_eq!(actual.to_string(), expected);
 }
