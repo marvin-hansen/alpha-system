d95f67c2397c896e03b493cec7189240a8d8299d
Marvin Hansen <marvin.hansen@gmail.com>
2023-12-19 21:15:16 +0800

    Added new client messages to QDGW
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ queng_services/qdgw/src/handle_data.rs
@@ -0,0 +1,38 @@
+use crate::service::Server;
+use common::prelude::MessageProcessingError;
+use sbe_messages::prelude::{StartDataMessage, StopAllDataMessage, StopDataMessage};
+
+impl Server {
+    pub(crate) async fn start_date(
+        &self,
+        start_data_msg: &StartDataMessage,
+    ) -> Result<(), MessageProcessingError> {
+        println!(
+            "[QDGW/handle::start_date]: start_data: {:?}",
+            start_data_msg
+        );
+
+        Ok(())
+    }
+
+    pub(crate) async fn stop_date(
+        &self,
+        stop_data_msg: &StopDataMessage,
+    ) -> Result<(), MessageProcessingError> {
+        println!("[QDGW/handle::stop_date]: stop_data: {:?}", stop_data_msg);
+
+        Ok(())
+    }
+
+    pub(crate) async fn stop_all_data(
+        &self,
+        stop_all_data_msg: &StopAllDataMessage,
+    ) -> Result<(), MessageProcessingError> {
+        println!(
+            "[QDGW/handle::stop_all_data]: stop_all_data: {:?}",
+            stop_all_data_msg
+        );
+
+        Ok(())
+    }
+}
