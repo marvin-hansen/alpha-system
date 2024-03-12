0b97e4acbbb28c01c7a09840e4d1abfda6cc61c5
Marvin Hansen <marvin.hansen@gmail.com>
2024-03-12 19:35:31 +0800

    Added insert query generator
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_cli/data_importer/src/bin/import_symbols/gen_query.rs
+++ queng_cli/data_importer/src/bin/import_symbols/gen_query.rs
@@ -1,4 +1,38 @@
+use lib_import::types::assets::{Asset, Metadata};
 use lib_import::types::exchanges::Exchange;
+
+pub fn generate_asset_insert(asset: &Asset) -> String {
+    let table_name = "default.assets";
+    let code = asset.code.clone();
+    let name = asset.name.clone();
+    let asset_class = asset.asset_class.clone();
+    let asset_figi = extract_asset_figi(&asset.metadata);
+
+    format!(
+        r"
+        INSERT INTO {table_name} (*)
+        VALUES (
+        '{code}',
+        '{name}',
+        '{asset_class}',
+        '{asset_figi}'
+        );
+    "
+    )
+}
+
+fn extract_asset_figi(metadata: &Option<Metadata>) -> String {
+    let empty_string = "".to_string();
+    let asset_figi = match metadata {
+        Some(metadata) => match &metadata.asset_figi {
+            Some(figi) => figi.to_owned(),
+            None => empty_string,
+        },
+        None => empty_string,
+    };
+
+    asset_figi
+}
 
 pub fn generate_exchange_insert(exchange: &Exchange) -> String {
     let table_name = "default.exchanges";
