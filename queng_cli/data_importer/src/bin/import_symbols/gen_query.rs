6aa8be76b8b41d5e2c42b7340030c7ca694178f3
Marvin Hansen <marvin.hansen@gmail.com>
2024-03-12 19:47:36 +0800

    Updated insert query generator
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_cli/data_importer/src/bin/import_symbols/gen_query.rs
+++ queng_cli/data_importer/src/bin/import_symbols/gen_query.rs
@@ -1,11 +1,12 @@
 use lib_import::types::assets::{Asset, Metadata};
 use lib_import::types::exchanges::Exchange;
+use lib_import::types::instruments::{Instrument, InstrumentMetadata};
 
 pub fn generate_asset_insert(asset: &Asset) -> String {
     let table_name = "default.assets";
-    let code = asset.code.clone();
-    let name = asset.name.clone();
-    let asset_class = asset.asset_class.clone();
+    let code = &asset.code;
+    let name = &asset.name;
+    let asset_class = &asset.asset_class;
     let asset_figi = extract_asset_figi(&asset.metadata);
 
     format!(
@@ -36,10 +37,10 @@ fn extract_asset_figi(metadata: &Option<Metadata>) -> String {
 
 pub fn generate_exchange_insert(exchange: &Exchange) -> String {
     let table_name = "default.exchanges";
-    let code = exchange.code.clone();
-    let name = exchange.name.clone();
+    let code = &exchange.code;
+    let name = &exchange.name;
     let active = exchange.active;
-    let url = exchange.url.clone().unwrap_or("".to_string());
+    let url = &exchange.url.clone().unwrap_or("".to_string());
     format!(
         r"
         INSERT INTO {table_name} (*)
@@ -51,4 +52,41 @@ pub fn generate_exchange_insert(exchange: &Exchange) -> String {
          );
     "
     )
+}
+
+pub fn generate_instruments_insert(instrument: &Instrument) -> String {
+    let table_name = "default.instruments";
+    let trade_start_timestamp = instrument.trade_start_timestamp.unwrap_or(0);
+    let trade_end_timestamp = instrument.trade_end_timestamp.unwrap_or(0);
+    let exchange_code = instrument.exchange_code();
+    let exchange_pair_code = &instrument.exchange_pair_code;
+    let base_asset = &instrument.base_asset;
+    let quote_asset = &instrument.quote_asset;
+    let code = &instrument.code;
+    let class = &instrument.class;
+    let (pair_figi, instrument_figi) = extract_instrument_figi(&instrument.metadata);
+
+    format!(
+        r"
+        INSERT INTO {table_name} (*)
+        VALUES (
+        {trade_start_timestamp},
+        {trade_end_timestamp},
+        '{exchange_code}',
+        '{exchange_pair_code}',
+        '{base_asset}',
+        '{quote_asset}',
+        '{code}',
+        '{class}',
+        '{pair_figi}',
+        '{instrument_figi}'
+        "
+    )
+    .to_string()
+}
+
+fn extract_instrument_figi(metadata: &Option<InstrumentMetadata>) -> (String, String) {
+    let empty_string = "".to_string();
+
+    (empty_string.clone(), empty_string.clone())
 }
