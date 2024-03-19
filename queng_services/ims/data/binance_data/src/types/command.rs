80cd76f18e52dafcd804dc40f39bba98f0ad8381
Marvin Hansen <marvin.hansen@gmail.com>
2024-03-19 15:00:30 +0800

    Cosmetic code changes.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_services/ims/data/binance_data/src/types/command.rs
+++ queng_services/ims/data/binance_data/src/types/command.rs
@@ -4,5 +4,4 @@ pub(crate) enum Command {
     StartData(u32, Vec<String>, DataType),
     StopData(u32),
     StopAllData,
-    ReconnectData(u32),
 }
--- queng_services/ims/data/binance_data/src/types/mod.rs
+++ queng_services/ims/data/binance_data/src/types/mod.rs
@@ -1,4 +1,3 @@
 pub(super) mod command;
 pub(super) mod symbols;
-
 // pub(crate) type Guarded<T> = Arc<RwLock<T>>;
