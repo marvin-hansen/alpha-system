d5dbb020ea4f6f0db00f6dfabfeb0bfb16f7db41
Marvin Hansen <marvin.hansen@gmail.com>
2024-03-21 11:35:23 +0800

    Updated Docker build shell script.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -14,6 +14,11 @@ command docker build -t dbgw:latest -f Dockerfile_dbgw .
 command rm Dockerfile_dbgw
 
 
+command cp queng_specs/service_specs/src/services/ims_data/binance/Dockerfile Dockerfile_bianance_data
+command docker build -t bianance_data:latest -f Dockerfile_bianance_data .
+command rm Dockerfile_bianance_data
+
+
 command cp queng_specs/service_specs/src/services/qdgw/Dockerfile Dockerfile_qdgw
 command docker build -t qdgw:latest -f Dockerfile_qdgw .
 command rm Dockerfile_qdgw
@@ -22,3 +27,7 @@ command rm Dockerfile_qdgw
 command cp queng_specs/service_specs/src/services/smdb/Dockerfile Dockerfile_smdb
 command docker build -t smdb:latest -f Dockerfile_smdb .
 command rm Dockerfile_smdb
+
+command cp queng_specs/service_specs/src/services/symdb/Dockerfile Dockerfile_symdb
+command docker build -t symdb:latest -f Dockerfile_symdb .
+command rm Dockerfile_symdb
