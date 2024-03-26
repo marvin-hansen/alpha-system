efee9f059f24b17578639a1762980431de6d749a
Marvin Hansen <marvin.hansen@gmail.com>
2024-03-27 06:25:10 +0800

    Added  gen ddl and gen query to import services.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_cli/data_importer/src/bin/import_services/gen_ddl.rs
+++ queng_cli/data_importer/src/bin/import_services/gen_ddl.rs
@@ -1,1 +1,35 @@
-
+pub(crate) fn generate_services_table_ddl() -> String {
+    r"
+    CREATE TABLE IF NOT EXISTS system.services
+    (
+        `svc_id` String CODEC(LZ4),
+        `name` String CODEC(LZ4),
+        `version` UInt8 CODEC(LZ4),
+        `online` Bool CODEC(LZ4),
+        `description` String CODEC(LZ4),
+        `health_check_uri` String CODEC(LZ4),
+        `base_uri` String CODEC(LZ4),
+        `dependencies` Array(Nullable(String)) CODEC(LZ4),
+        `exposure` String CODEC(LZ4),
+        `endpoint`  Tuple(
+            description CODEC(LZ4),
+            encoding CODEC(LZ4),
+            name Nullable(String),
+            port UInt32 CODEC(Delta, LZ4),
+            protocol String CODEC(LZ4),
+            uri String CODEC(LZ4),
+            version UInt8 CODEC(LZ4)
+        ) CODEC(LZ4),
+        `metrics`  Tuple(
+            metric_host CODEC(LZ4),
+            metric_port UInt32 CODEC(Delta, LZ4),
+            metric_uri CODEC(LZ4)
+        ) CODEC(LZ4),
+    )
+    ENGINE = MergeTree
+    PRIMARY KEY (svc_id)
+    ORDER BY (svc_id)
+    SETTINGS index_granularity = 1024
+    "
+    .to_string()
+}
