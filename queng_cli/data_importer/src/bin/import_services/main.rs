f6738fd4bd7c72756ad454152bc0403f77ee7497
Marvin Hansen <marvin.hansen@gmail.com>
2024-03-27 05:51:47 +0800

    Started working on importing service meta data into clickhouse
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ queng_cli/data_importer/src/bin/import_services/main.rs
@@ -0,0 +1,12 @@
+use service_specs::services::get_all_service_configs;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let services = get_all_service_configs();
+
+    for service in services {
+        println!("{}", service);
+    }
+
+    Ok(())
+}
