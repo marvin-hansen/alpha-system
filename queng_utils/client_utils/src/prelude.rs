3aaadef509ef77a57544df138bb8bcf20f4934ce
Marvin Hansen <marvin.hansen@gmail.com>
2024-03-28 12:12:40 +0800

    Configured doc & doc test for all remaining crates.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_utils/client_utils/src/prelude.rs
+++ queng_utils/client_utils/src/prelude.rs
@@ -1,7 +1,6 @@
 pub use crate::config_utils;
 pub use crate::counter_utils::atomic_counter;
 pub use crate::csv_utils;
-pub use crate::data_utils;
 pub use crate::file_utils;
 pub use crate::print_utils;
 pub use crate::symbol_utils;
