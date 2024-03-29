ee43b5205c5b89d14dc6475833f468cd624ede2a
Marvin Hansen <marvin.hansen@gmail.com>
2024-03-29 20:30:33 +0800

    Updated Bazel remote config. Updated Bazel build scripts used in the Makefile.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/bazel/push.sh
+++ scripts/bazel/push.sh
@@ -3,7 +3,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-command bazel run //queng_services/cmdb:push
-command bazel run //queng_services/ims/data/binance_data:push
-command bazel run //queng_services/smdb:push
-command bazel run //queng_services/symdb:push
+command command cargo fmt --all
+
+# Compile everything in release mode
+bazel build  --nolegacy_important_outputs \
+             --noslim_profile \
+             --experimental_remote_cache_compression \
+             --experimental_profile_include_target_label \
+             --experimental_profile_include_primary_output \
+             -c opt //...
+
+# Run all tests
+bazel test   --nolegacy_important_outputs \
+             --noslim_profile \
+             --experimental_remote_cache_compression \
+             --experimental_profile_include_target_label \
+             --experimental_profile_include_primary_output \
+             -c opt //...
+
+## Publish all Docker images to the registry
+#command bazel run -c opt //queng_services/cmdb:push
+#command bazel run -c opt //queng_services/ims/data/binance_data:push
+#command bazel run -c opt //queng_services/smdb:push
+#command bazel run -c opt //queng_services/symdb:push
+
+# Push all remaining commits to remote
+command git push
