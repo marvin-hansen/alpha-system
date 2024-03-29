fbf4f7943e5e32ccf190d5a3e5a00d593c0545ac
Marvin Hansen <marvin.hansen@gmail.com>
2024-03-29 21:15:57 +0800

    Cosmetic changes
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_utils/db_utils/src/types.rs
+++ queng_utils/db_utils/src/types.rs
@@ -1,7 +1,8 @@
 use klickhouse::Row;
 use serde::{Deserialize, Serialize};
+use std::fmt::Display;
 
-#[derive(Debug, Row, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Row, Serialize, Deserialize)]
 pub struct CountRow {
     count: u64,
 }
@@ -9,5 +10,11 @@ pub struct CountRow {
 impl CountRow {
     pub fn count(&self) -> u64 {
         self.count
+    }
+}
+
+impl Display for CountRow {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.count)
     }
 }
