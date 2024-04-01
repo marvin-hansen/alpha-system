77d6dd69773284fbdcb616e677443e7aea33deab
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-01 13:47:36 +0800

    Fixed spec tests
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_specs/service_specs/tests/services/dbgw_tests.rs
+++ queng_specs/service_specs/tests/services/dbgw_tests.rs
@@ -15,7 +15,7 @@ fn test_cmdb_service_config() {
     );
     assert_eq!(
         service_config.health_check_uri(),
-        "dbgwv1-service.default.svc.cluster.local:6060/health"
+        "dbgwv1-service.default.svc.cluster.local:7070/health"
     );
     assert_eq!(
         service_config.base_uri(),
@@ -29,10 +29,10 @@ fn test_cmdb_service_config() {
     assert_eq!(endpoint.version(), 1);
     assert_eq!(
         endpoint.description(),
-        "Access to surreal DB via gRPC on baseUri:6060"
+        "Access to surreal DB via gRPC on baseUri:7070"
     );
     assert_eq!(endpoint.uri(), String::from("/"));
-    assert_eq!(endpoint.port(), 6060);
+    assert_eq!(endpoint.port(), 7070);
     assert_eq!(endpoint.protocol(), ProtocolType::GRPC);
     assert_eq!(endpoint.encoding(), Encoding::Protobuf);
 }
