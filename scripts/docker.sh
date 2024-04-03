67b1da572f8a4c18c061f5964d4a795b659dc2b6
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-03 15:48:10 +0800

    Separated Dockerfile into two, one for local and another one optimized for cloud build.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,10 +3,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=cmdb"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/cmdb_arm64:latest -f Dockerfile .
-command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=dbgw"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/dbgw_arm64:latest -f Dockerfile .
-command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=smdb"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/smdb_arm64:latest -f Dockerfile .
-command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=symdb" --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/symdb_arm64:latest -f Dockerfile .
+command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=cmdb"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/cmdb_arm64:latest -f Dockerfile_Local .
+command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=dbgw"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/dbgw_arm64:latest -f Dockerfile_Local .
+command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=smdb"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/smdb_arm64:latest -f Dockerfile_Local .
+command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=symdb" --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/symdb_arm64:latest -f Dockerfile_Local .
 
 command docker push asia-northeast1-docker.pkg.dev/future-309012/image-repo/cmdb_arm64:latest
 command docker push asia-northeast1-docker.pkg.dev/future-309012/image-repo/dbgw_arm64:latest
--- scripts/release.sh
+++ scripts/release.sh
@@ -41,10 +41,10 @@ bazel build  --nolegacy_important_outputs \
 
 # Build and publish all Docker images to the registry
 
-command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=cmdb"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/cmdb_arm64:latest -f Dockerfile .
-command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=dbgw"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/dbgw_arm64:latest -f Dockerfile .
-command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=smdb"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/smdb_arm64:latest -f Dockerfile .
-command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=symdb" --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/symdb_arm64:latest -f Dockerfile .
+command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=cmdb"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/cmdb_arm64:latest -f Dockerfile_Local .
+command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=dbgw"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/dbgw_arm64:latest -f Dockerfile_Local .
+command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=smdb"  --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/smdb_arm64:latest -f Dockerfile_Local .
+command docker build --platform=linux/arm64 --build-arg="SERVICE_NAME=symdb" --build-arg="BUILD_TARGET=aarch64-unknown-linux-musl" -t asia-northeast1-docker.pkg.dev/future-309012/image-repo/symdb_arm64:latest -f Dockerfile_Local .
 
 command docker push asia-northeast1-docker.pkg.dev/future-309012/image-repo/cmdb_arm64:latest
 command docker push asia-northeast1-docker.pkg.dev/future-309012/image-repo/dbgw_arm64:latest
