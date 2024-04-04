dad645ce1ed3e2dbbbc9a660661e9208785ae9a5
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-04 23:29:31 +0800

    Updated Bazel config.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,9 +6,6 @@ set -o pipefail
 
 bazel test --bes_results_url=https://app.buildbuddy.io/invocation/ \
              --bes_backend=grpcs://remote.buildbuddy.io \
-             --nolegacy_important_outputs \
-             --noslim_profile \
-             --experimental_remote_cache_compression \
              --experimental_profile_include_target_label \
              --experimental_profile_include_primary_output \
              //...
