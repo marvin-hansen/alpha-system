e874874510f430441bc626982eec0cc246f25f74
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-07 13:16:36 +0800

    updated setup auth script.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- buildbuddy_setup_docker_auth.sh
+++ buildbuddy_setup_docker_auth.sh
@@ -1,11 +1,10 @@
-# bin/sh
-set -o errexit
-set -o nounset
-set -o pipefail
+#!/usr/bin/env bash
 set -e
 
 mkdir -p ~/.docker
+
 AUTH=$(echo -n "_json_key:$SERVICE_ACCOUNT_KEY_JSON" | base64 -w 0)
+
 cat > ~/.docker/config.json <<EOF
 {"auths": {"gcr.io": {"auth": "$AUTH"}}}
 EOF
