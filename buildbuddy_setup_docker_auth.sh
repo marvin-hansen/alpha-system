55df898baed7abd49adadc273609d393b4deb3a7
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-07 13:36:04 +0800

    Working on fixing CI.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- buildbuddy_setup_docker_auth.sh
+++ buildbuddy_setup_docker_auth.sh
@@ -6,5 +6,5 @@ mkdir -p ~/.docker
 AUTH=$(echo -n "_json_key:$SERVICE_ACCOUNT_KEY_JSON" | base64 -w 0)
 
 cat > ~/.docker/config.json <<EOF
-{"auths": {"gcr.io": {"auth": "$AUTH"}}}
+{"auths": {"asia-northeast1-docker.pkg.dev": {"auth": "$AUTH"}}}
 EOF
