5664570925a1cf24d01dd0bcee7762a76552eacf
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-09 19:56:31 +0800

    Fixed make container script
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/container.sh
+++ scripts/container.sh
@@ -5,7 +5,5 @@ set -o pipefail
 
 # Builds all images
 command bazel build -c opt  //:image
-# Generates tags for all images
-command bazel build //:build_stamp_tag
 # Pushes all tagged images to registry
-command baze run -c opt //:push --stamp --embed_label="$TAG"
+command baze run -c opt //:push --workspace_status_command 'tools/workspace_status.sh'
