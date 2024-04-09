010e3664f900c27dcc5a7945f4fef3906e5c7e60
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-09 12:06:09 +0800

    Working on CI config
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- build_stamp_tag.sh
+++ build_stamp_tag.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-export TAG="$(date +"%Y.%m.%d")"."$(git rev-parse --short HEAD)"
-export BUILD_EMBED_LABEL="$TAG"
+export TAG="$(date +"%Y.%m.%d")"."$(git rev-parse --short HEAD)"export BUILD_EMBED_LABEL="$TAG"
+git tag $TAG HEAD
+git push --tags
+
 echo TAG "$TAG"
-echo BUILD_EMBED_LABEL "$BUILD_EMBED_LABEL"
