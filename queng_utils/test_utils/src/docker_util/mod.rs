f9cc287aede761b90616330cdf538d6e78f0414d
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-15 07:45:39 +0800

    Working on Docker utils
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_utils/test_utils/src/docker_util/mod.rs
+++ queng_utils/test_utils/src/docker_util/mod.rs
@@ -1,30 +1,45 @@
 use crate::prelude::DockerError;
+use std::collections::HashMap;
 
 use docker_engine_api::client::{Client, ClientTrait};
+use docker_engine_api::container_create::CreateContainerFrom;
+use docker_engine_api::containers_service::ContainersServiceTrait;
+
+const DEFAULT_PLATFORM: &str = "linux";
 
 pub struct DockerUtil {
     client: Client,
 }
 
 impl DockerUtil {
-    pub fn new() -> Self {
-        let mut client = docker_engine_api::new("/var/run/docker.sock".to_string());
+    pub fn new() -> Result<Self, DockerError> {
+        let client = docker_engine_api::new("/var/run/docker.sock".to_string());
         match client.ping() {
-            Ok(_) => {
-                println!("Pong!")
+            Ok(_) => {}
+            Err(e) => {
+                println!("Failed to connect to Docker: {}", e);
+                return Err(DockerError::from(e.to_string()));
             }
-            Err(e) => panic!("Error: {}", e),
         };
 
-        Self { client }
+        Ok(Self { client })
     }
 }
 
 impl DockerUtil {
     /// Check if a container is running
     ///
-    pub fn is_container_running(&self, name: &str) -> Result<bool, DockerError> {
-        return Err(DockerError::from("NOT IMPLEMENTED"));
+    pub fn check_if_container_exists(&mut self, name: &str) -> Result<bool, DockerError> {
+        // Somehow the client API has no way build in to check if a container is exists.
+        // So we have to improvise by calling the stats endpoint b/c no container, no stats...
+        return match self
+            .client
+            .containers
+            .get_stats_container(name, false, true)
+        {
+            Ok(_) => Ok(true),
+            Err(_) => Ok(false),
+        };
     }
 
     pub fn get_running_container(&self) -> Result<(u16, String), DockerError> {
@@ -33,46 +48,69 @@ impl DockerUtil {
 
     /// Start a container
     pub fn start_container(
-        &self,
+        &mut self,
         name: &str,
         image: &str,
         port: u16,
         reuse_server: bool,
     ) -> Result<(u16, String), DockerError> {
         //
-        // Check if container is already running
-        let running = self
-            .is_container_running(name)
-            .expect("Failed to check if container is running");
+        // Check if container already exists.
+        let exists = self
+            .check_if_container_exists(name)
+            .expect("Failed to check if container exists");
 
-        if reuse_server && running {
-            let (port, container_name) = match self.get_running_container() {
-                Ok((port, container_name)) => (port, container_name),
-                Err(e) => return Err(e),
-            };
+        // If so, check if we can re-use it
+        if exists {
+            // Check if container is already running
+            let running = self
+                .check_if_container_exists(name)
+                .expect("Failed to check if container is running");
 
-            return Ok((port, container_name));
+            if reuse_server && running {
+                let (port, container_name) = match self.get_running_container() {
+                    Ok((port, container_name)) => (port, container_name),
+                    Err(e) => return Err(e),
+                };
+
+                return Ok((port, container_name));
+            }
+
+            // Because we don't re-use the server, we need to stop the container first
+            if running {
+                self.stop_container(name).expect("Failed to stop container");
+            }
         }
 
-        // Because we don't re-use the server, we need to stop the container first
-        if running {
-            self.stop_container(name).expect("Failed to stop container");
-        }
+        // Container doesn't exist, so let's create one.
+        // Define options
+        let mut options = CreateContainerFrom::default();
+        options.image = Some(image.to_string());
+        options.cmd = Some(vec!["/bin/true".to_string()]);
 
-        // Start the container
+        // Define exposed ports
+        let mut exposed_ports = HashMap::new();
+        exposed_ports.insert(port.to_string(), ());
+        exposed_ports.insert("8080/tcp".to_string(), ());
+        options.exposed_ports = Some(exposed_ports);
 
-        return Err(DockerError::from("NOT IMPLEMENTED"));
+        // Call API
+        return match self
+            .client
+            .containers
+            .create_container(name, DEFAULT_PLATFORM, &options)
+        {
+            Ok(_) => Ok((port, name.to_string())),
+            Err(e) => Err(DockerError::from(e.to_string())),
+        };
     }
 
     /// Stop a container
-    pub fn stop_container(&self, name: &str) -> Result<(), DockerError> {
+    pub fn stop_container(&self, _name: &str) -> Result<(), DockerError> {
         return Err(DockerError::from("NOT IMPLEMENTED"));
     }
-}
 
-impl DockerUtil {
-    /// Find a free port
-    fn find_free_port() -> u16 {
-        0
+    pub fn remove_container(&self, _name: &str) -> Result<(), DockerError> {
+        return Err(DockerError::from("NOT IMPLEMENTED"));
     }
 }
--- queng_utils/test_utils/tests/docker_utils/mod.rs
+++ queng_utils/test_utils/tests/docker_utils/mod.rs
@@ -1,33 +1,51 @@
 use test_utils::prelude::DockerUtil;
 
-#[test]
-fn test_is_container_running() {
-    let docker_util = DockerUtil::new();
-    let result = docker_util.is_container_running("test_container");
+fn get_docker_util() -> DockerUtil {
+    DockerUtil::new().expect("Failed to create DockerUtil")
+}
 
+#[test]
+fn test_new() {
+    let result = DockerUtil::new();
     assert!(result.is_ok());
 }
 
 #[test]
-fn test_get_running_container() {
-    let docker_util = DockerUtil::new();
-    let running_container = docker_util.get_running_container();
-
-    assert!(running_container.is_ok())
-}
-
-#[test]
 fn test_start_container() {
-    let docker_util = DockerUtil::new();
+    let mut docker_util = get_docker_util();
     let result = docker_util.start_container("test_container", "test_image", 8080, false);
 
     assert!(result.is_ok())
 }
 
 #[test]
+fn test_get_container_status() {
+    let mut docker_util = get_docker_util();
+    let result = docker_util.check_if_container_exists("test_container");
+
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_get_running_container() {
+    let docker_util = get_docker_util();
+    let running_container = docker_util.get_running_container();
+
+    assert!(running_container.is_ok())
+}
+
+#[test]
 fn test_stop_container() {
-    let docker_util = DockerUtil::new();
+    let docker_util = get_docker_util();
     let result = docker_util.stop_container("test_container");
+
+    assert!(result.is_ok())
+}
+
+#[test]
+fn test_remove_container() {
+    let docker_util = get_docker_util();
+    let result = docker_util.remove_container("test_container");
 
     assert!(result.is_ok())
 }
