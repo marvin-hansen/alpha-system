9ee660ba88a6ab80ddda8b8f13b18be917792739
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-17 10:20:35 +0800

    Rewrote Docker util using only cli commands.
    Removed dependency on Docker API.
    
    Refactored code.
    
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_utils/test_utils/tests/docker_utils/setup/mod.rs
+++ queng_utils/test_utils/tests/docker_utils/setup/mod.rs
@@ -3,37 +3,27 @@ use test_utils::prelude::DockerUtil;
 pub fn get_docker_util() -> DockerUtil {
     DockerUtil::new().expect("Failed to create DockerUtil")
 }
-//
-//
-// #[test]
-// fn test_new() {
-//     let result = DockerUtil::new();
-//     assert!(result.is_ok());
-// }
 
 #[test]
-fn test_start_container_no_reuse() {
-    let mut docker_util = get_docker_util();
-    let result = docker_util.start_container(
-        "test_container",
-        "alpine:latest",
-        Some(vec!["/bin/true".to_string()]),
-        7070,
-        false,
-    );
+fn test_new() {
+    let result = DockerUtil::new();
 
-    assert!(result.is_ok())
+    if result.is_err() {
+        println!("{}", result.as_ref().unwrap_err());
+    }
+
+    assert!(result.is_ok());
 }
-//
+
 // #[test]
-// fn test_start_container_reuse_running_container() {
+// fn test_start_container_no_reuse() {
 //     let mut docker_util = get_docker_util();
 //     let result = docker_util.start_container(
 //         "test_container",
-//         "test_image",
+//         "alpine:latest",
 //         Some(vec!["/bin/true".to_string()]),
 //         7070,
-//         true,
+//         false,
 //     );
 //
 //     assert!(result.is_ok())
