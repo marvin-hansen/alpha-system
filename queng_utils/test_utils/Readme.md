c7431f044c05a398d042116cccd232d8fe6e80c4
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-18 12:45:36 +0800

    Completed Docker util with Docs and examples.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ queng_utils/test_utils/Readme.md
@@ -0,0 +1,8 @@
+# Test Utils
+
+## Docker Utils
+
+Docker utils help to create, re-use, or stop containers during tests. Requires 
+a Docker installation with a working CLI. 
+
+For an usage example, see [src_docker_bin](src/docker_bin). 
---
+++ queng_utils/test_utils/src/types/container_config.rs
@@ -0,0 +1,65 @@
+use std::fmt::Display;
+
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct ContainerConfig<'l> {
+    name: &'l str,
+    image: &'l str,
+    port: u16,
+    reuse_container: bool,
+}
+
+impl<'l> ContainerConfig<'l> {
+    /// Create a new instance of the `ContainerConfig` struct with the given parameters.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - The name of the container.
+    /// * `image` - The image to use for the container.
+    /// * `port` - The port number for the container.
+    /// * `reuse_container` - A boolean flag indicating whether to reuse an existing container if found.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new instance of the `ContainerConfig` struct.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use test_utils::prelude::ContainerConfig;
+    ///
+    /// let container_config = ContainerConfig::new("my_container", "nginx:latest", 80, false);
+    /// ```
+    pub fn new(name: &'l str, image: &'l str, port: u16, reuse_container: bool) -> Self {
+        Self {
+            name,
+            image,
+            port,
+            reuse_container,
+        }
+    }
+}
+
+impl<'l> ContainerConfig<'l> {
+    pub fn name(&self) -> &'l str {
+        self.name
+    }
+    pub fn image(&self) -> &'l str {
+        self.image
+    }
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+    pub fn reuse_container(&self) -> bool {
+        self.reuse_container
+    }
+}
+
+impl Display for ContainerConfig<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "name: {}, image: {}, port: {}, reuse_container: {}",
+            self.name, self.image, self.port, self.reuse_container
+        )
+    }
+}
