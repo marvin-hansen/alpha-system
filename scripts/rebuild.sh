aeb52889bc028610f8ed5914a76516c29571da77
Marvin Hansen <marvin.hansen@gmail.com>
2024-04-26 08:46:44 -0500

    Updated scripts.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -6,13 +6,8 @@ set -o pipefail
 # Syncs Cargo dependencies to Bazel index
 CARGO_BAZEL_REPIN=true bazel build //...
 
-# Compile everything
-command bazel build //... --jobs=50
+# Compile everything again using the updated dependencies
+command bazel build //...
 
-# Run all tests & upload results to BES
-command bazel \
-             test \
-             --bes_results_url=https://app.buildbuddy.io/invocation/ \
-             --bes_backend=grpcs://remote.buildbuddy.io \
-             //... \
-             --jobs=50
+# Run all tests again to double check everything is okay.
+command bazel test //...
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,3 @@ set -o nounset
 set -o pipefail
 
 command bazel test //...
-
-#command bazel \
-#             test \
-#             --bes_results_url=https://app.buildbuddy.io/invocation/ \
-#             --bes_backend=grpcs://remote.buildbuddy.io \
-#             //... \
-#             --jobs=50
