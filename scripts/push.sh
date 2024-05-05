fc88b5a4bb343a267250a061ae643d0c5f9aae1a
Marvin Hansen <marvin.hansen@gmail.com>
2024-05-05 20:10:49 +0800

    Updated push script.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,20 +6,10 @@ set -o pipefail
 # Format all source code.
 command command cargo fmt --all
 
-# Kick off remote build and test in the background to speed up CI
-command bazel build -c opt //... --config=remote &
-command bazel test  -c opt //... --config=remote &
+# Kick off remote build and test with bazel to ensure hot cache for CI
+command bazel build -c opt //... --config=remote
 
-# Compile everything
-command bazel build //...
-
-# Build all docs
-command bazel build //:doc
-
-# Run all tests & upload results to BES
-command bazel test --bes_results_url=https://app.buildbuddy.io/invocation/ \
-             --bes_backend=grpcs://remote.buildbuddy.io \
-             //...
+command bazel test  -c opt //... --test_tag_filters=unit --config=remote
 
 # Push all new and remaining commits to remote to trigger CI
 command git push
