96fdafb1102aa83f28680a326017279e24e33005
Marvin Hansen <marvin.hansen@gmail.com>
2024-06-19 13:29:00 +0800

    Added MUSL build with custom memory allocator to DBGW.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_services/dbgw/src/main.rs
+++ queng_services/dbgw/src/main.rs
@@ -4,6 +4,7 @@ use ctx_manager::CtxManager;
 use db_system_manager::SystemDBManager;
 use dbgw_specs;
 use dns_manager::DnsManager;
+use jemallocator::Jemalloc;
 use proto_bindings::proto::db_gateway_service_server::DbGatewayServiceServer;
 use service::DBGWServer;
 use service_utils::{print_utils, shutdown_utils};
@@ -12,6 +13,12 @@ use tonic::transport::Server;
 
 mod service;
 const SVC_ID: ServiceID = ServiceID::DBGW;
+
+// Jemalloc overwrites the default memory allocator.
+// This fixes a performance issue due to threat contention in the MUSL memory allocator.
+// https://www.linkedin.com/pulse/testing-alternative-c-memory-allocators-pt-2-musl-mystery-gomes
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
