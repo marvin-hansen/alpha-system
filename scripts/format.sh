7804f53bee0ae942cab337db8bd3cf5afa888349
Marvin Hansen <marvin.hansen@gmail.com>
2024-06-28 17:23:20 +0800

    Updated formatting script to format all Bazel files.
    Formatted Bazel files.
    
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,6 +4,10 @@ set -o nounset
 set -o pipefail
 
 
-# Code formatting
+# Bazel file formatting (Installed via homebrew)
+# https://github.com/bazelbuild/buildtools
+buildifier -r queng_* MODULE.bazel BUILD.bazel WORKSPACE.bzlmod
+
+# Rust code formatting
 # https://github.com/rust-lang/rustfmt
 command cargo fmt --all
