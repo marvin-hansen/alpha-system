530bab9f13f8ff7381e78b0f0cb01b61da023e5a
Marvin Hansen <marvin.hansen@gmail.com>
2024-06-30 21:29:41 +0800

    Trigger CI
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- buildbuddy_setup_docker_auth.sh
+++ buildbuddy_setup_docker_auth.sh
@@ -5,11 +5,11 @@ mkdir -p ~/.docker
 
 GCP_AUTH=$(echo -n "_json_key:$CONTAINER_REGISTRY_SERVICE_ACCOUNT_KEY_JSON" | base64 -w 0)
 
-DOCKER_AUTH=$(echo -n "$DOCKER_HUB_TOKEN" | base64 -w 0)
+DOCKER_AUTH=$(echo -n "$DOCKER_HUB_TOKEN" | base64)
 
 cat > ~/.docker/config.json <<EOF
 {"auths":
   {"asia-northeast1-docker.pkg.dev": {"auth": "$GCP_AUTH"}},
-  {"https://index.docker.io/v1/": {"auth": "$DOCKER_AUTH"}}
+  {"https://index.docker.io/v1/": {"auth": "$DOCKER_AUTH"}},
 }
 EOF
