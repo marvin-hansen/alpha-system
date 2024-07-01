af5d918f3f0af0ec0cc67e356bd69b3fa0ee4aa5
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-01 21:37:03 +0800

    Cleaned up service utils.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_utils/service_utils/src/lib.rs
+++ queng_utils/service_utils/src/lib.rs
@@ -1,29 +1,2 @@
-use std::fmt::Debug;
-
 pub mod print_utils;
 pub mod shutdown_utils;
-
-#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
-pub struct ServiceUtil {
-    dbg: bool,
-}
-
-impl ServiceUtil {
-    pub fn new() -> Self {
-        Self::build(false)
-    }
-
-    pub fn with_debug() -> Self {
-        Self::build(true)
-    }
-
-    fn build(dbg: bool) -> Self {
-        Self { dbg }
-    }
-}
-
-impl Default for ServiceUtil {
-    fn default() -> Self {
-        Self::new()
-    }
-}
