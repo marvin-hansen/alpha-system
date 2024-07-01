ff266475c8ceb791bbea5fb2e7ea4ba87bbf7c6b
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-01 20:58:37 +0800

    Trigger CI
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_utils/docker_utils/tests/docker_test_docker_hub.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-set -o nounset
-set -o pipefail
-
-docker pull --platform linux/amd64 hansenmarvin/api_proxy:b422ae3
--- queng_utils/docker_utils/tests/docker_test_gcp.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-set -o nounset
-set -o pipefail
-
-docker pull --platform linux/amd64 asia-northeast1-docker.pkg.dev/future-309012/image-repo/api_proxy:b422ae3
-
