55c79ea2f7dd6b767d3841dc0e23d14d22b6410d
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-08 19:19:15 +0800

    Rewrote CH util test suite with proper CI setup with EnvUtil.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_utils/clickhouse_utils/tests/clickhouse_utils_tests.rs
+++ queng_utils/clickhouse_utils/tests/clickhouse_utils_tests.rs
@@ -1,39 +1,27 @@
 use clickhouse_utils::ClickhouseUtil;
-use container_specs::clickhouse_container_config::clickhouse_container_config;
-use docker_utils::DockerUtil;
-use std::{thread, time};
+use env_utils::EnvUtil;
+
+use std::{env, time};
+use tokio::time::sleep;
+
+// Starts a kaiko api proxy on localhost port 7777
+async fn setup_ci_env() {
+    // Set the environment variable.
+    env::set_var("ENV", "CI");
+
+    // Create new Env Utils
+    let mut ci_env = EnvUtil::with_debug().await.expect("Failed to get EnvUtil");
+
+    // Initiate CI container
+    ci_env
+        .setup_container_clickhouse()
+        .await
+        .expect("Failed to setup clickhouse container");
+}
 
 #[tokio::test]
-async fn docker_env_util_setup_db_test() {
-    // Create a new DockerUtil in debug mode. Without debug, just call new()
-    let docker_util = DockerUtil::with_debug().expect("Failed to create DockerUtil");
-    // Set default wait duration
-    let wait_time = time::Duration::from_millis(500);
-
-    println!();
-    println!(">> Test get_or_start_container: Create a new container");
-    println!();
-
-    // Create a new container from config and start or reuse it
-    let container_config = clickhouse_container_config();
-    let result = docker_util.get_or_start_container_config(&container_config);
-    if result.is_err() {
-        println!("{}", result.as_ref().unwrap_err());
-    }
-
-    // Pause execution to check Docker UI/CLI if the container is up & running
-    thread::sleep(wait_time);
-
-    assert!(result.is_ok());
-
-    let (container_name, port) = result.unwrap();
-    assert_eq!(container_name, "clickhouse-9000");
-    assert_eq!(port, 9000);
-
-    let exists = docker_util
-        .check_if_container_exists(&container_name)
-        .expect("Failed to check if container exists");
-    assert!(exists);
+async fn setup_db_test() {
+    setup_ci_env().await;
 
     let dsn = "127.0.0.1:9000".to_string();
     let result = ClickhouseUtil::new(dsn).await;
@@ -45,7 +33,8 @@ async fn docker_env_util_setup_db_test() {
     let setup_result = ch_utils.setup_db().await;
     assert!(setup_result.is_ok());
 
-    thread::sleep(wait_time);
+    let wait_time = time::Duration::from_millis(500);
+    sleep(wait_time).await;
 
     let teardown_result = ch_utils.teardown_db().await;
     assert!(teardown_result.is_ok());
