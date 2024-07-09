8c7929a14b51ea10803c59a2de5195d8165cb787
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-09 17:13:10 +0800

    Fixed test in SystemDBManager
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_components/db_system_manager/tests/db_system_manager_tests.rs
+++ queng_components/db_system_manager/tests/db_system_manager_tests.rs
@@ -1,16 +1,11 @@
-use common::prelude::{ClickHouseConfig, EnvironmentType, ServiceID};
-use config_manager::CfgManager;
+use common::prelude::{ClickHouseConfig, EnvironmentType};
 use ctx_manager::CtxManager;
 use db_system_manager::SystemDBManager;
-use dns_manager::DnsManager;
 use env_utils::prelude::EnvUtil;
-use smdb_specs::smdb_service_config;
 use std::env;
+
 async fn setup_env() {
-    // Do the initial setup
-    // Set the environment variable.
-    env::set_var("ENV", "CI");
-    // Internal CI DNS server.
+    env::set_var("ENV", "CLUSTER");
     env::set_var("DNS_SERVER", "9.9.9.9");
 }
 
@@ -26,17 +21,7 @@ async fn test_new() {
     // Build & configure components for contextual autoconfiguration.
     // Context manager determines the environment type.
     let ctxm = CtxManager::new();
-    assert_eq!(ctxm.env_type(), EnvironmentType::CI);
-
-    // Build & configure components for DNS autoconfiguration relative to the environment type.
-    let dnm = DnsManager::new(&ctxm);
-    assert_eq!(dnm.internal_dns_server(), "9.9.9.9:53");
-    assert_eq!(dnm.external_dns_server(), "1.1.1.1:53");
-
-    // Configure manager for context aware auto configuration.
-    let config_manager = CfgManager::new(ServiceID::SMDB, smdb_service_config(), &ctxm, &dnm);
-    assert_eq!(config_manager.get_svc_id(), ServiceID::SMDB);
-    assert_eq!(config_manager.get_env_type(), EnvironmentType::CI);
+    assert_eq!(ctxm.env_type(), EnvironmentType::CLUSTER);
 
     let clickhouse_config = ClickHouseConfig::new(
         "127.0.0.1".to_string(),
