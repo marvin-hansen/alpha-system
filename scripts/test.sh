e4f043258f47645a24ac3cb8735c52f85ceed175
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-11 19:16:31 +0800

    Updated test script and BB config.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# When adding new bazel commands, also update buildbuddy.yaml with the same command for CI testing
+
+command bazel build //...
+
 echo "=============="
 echo "Run doc tests"
 echo "=============="
@@ -16,13 +20,27 @@ command bazel build //... --build_tag_filters=unit
 command bazel test //... --test_tag_filters=unit
 
 echo "=============="
-echo "Setup Test Env"
+echo "Setup Containers"
 echo "=============="
-command bazel test //... --test_tag_filters=env_setup
+command bazel test //... --test_tag_filters=env_container_setup
 
 echo "====================="
 echo "Run integration tests"
 echo "====================="
-# First check if the utils are working as expected before running the integration tests
-command bazel test //... --test_tag_filters=clickhouse_utils
-# Then run the integration tests for the DB components
+
+echo ""
+
+echo "====================="
+echo "Test: API Proxy integration"
+echo "====================="
+command bazel test //... --test_tag_filters=clickhouse_utils_integration_tests
+
+echo "====================="
+echo "Test: Clickhouse Integration"
+echo "====================="
+command bazel test //... --test_tag_filters=clickhouse_utils_integration_tests
+
+echo "====================="
+echo "Build container images"
+echo "====================="
+command bazel build //:push
