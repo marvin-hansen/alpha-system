c9618924404fb37002d93f012afee0c1a8f3a700
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-14 10:55:40 +0800

    Moved common math utils into a dedicated math_utils crate.
    Updated dependencies all callsites accordingly.
    
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_common/src/prelude.rs
+++ queng_common/src/prelude.rs
@@ -61,6 +61,3 @@ pub use crate::types::trade_types::strategy_config::*;
 pub use crate::types::trade_types::trade_direction::*;
 pub use crate::types::trade_types::trade_entry::*;
 pub use crate::types::trade_types::trade_strategy::*;
-//
-// Utils
-pub use crate::utils::math_utils;
