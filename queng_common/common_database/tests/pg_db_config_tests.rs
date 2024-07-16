c00cb9452cf81aba327469231ecca78c8dafe080
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-16 12:12:40 +0800

    Added test for PostgresDBConfig
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_common/common_database/tests/pg_db_config_tests.rs
+++ queng_common/common_database/tests/pg_db_config_tests.rs
@@ -1,1 +1,22 @@
+use common_database::prelude::PostgresDBConfig;
 
+#[test]
+fn test_postgres_db_config() {
+    // Setup - create an instance of PostgresDBConfig with known values
+    let config = PostgresDBConfig::new(
+        "localhost".to_string(),
+        "user".to_string(),
+        "password".to_string(),
+        "test_db".to_string(),
+        5432,
+        10,
+    );
+
+    // Exercise & Verify - test each accessor method
+    assert_eq!(config.pg_host(), "localhost");
+    assert_eq!(config.pg_user(), "user");
+    assert_eq!(config.pg_password(), "password");
+    assert_eq!(config.pg_database(), "test_db");
+    assert_eq!(config.pg_port(), 5432);
+    assert_eq!(config.pg_max_connections(), 10);
+}
