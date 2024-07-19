3409413305f2625cbc12950d3dae356a27743edf
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-19 10:27:08 +0800

    Updated check script
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/check.sh
+++ scripts/check.sh
@@ -8,10 +8,29 @@ set -o pipefail
 # https://github.com/kbknapp/cargo-outdated
 command cargo outdated --workspace
 
+
+# Scan for unused dependencies
+# https://crates.io/crates/cargo-udeps
+command cargo +nightly udeps --all-targets
+
+
+# Check a package and all of its dependencies for errors.
+# https://doc.rust-lang.org/cargo/commands/cargo-check.html
+command cargo check --all-targets
+
+
 # Check for linter errors
 # https://github.com/rust-lang/rust-clippy
 command cargo clippy --all-targets
 
+
 # Check code formatting
 # https://github.com/rust-lang/rustfmt
-#command cargo fmt --all --check
+command cargo fmt --all --check
+
+
+# The problem of sub-sub dependencies simply not patching known vulnerabilities
+# remains unsolved hence sec scanning has been disabled for the time being.
+# Scan for security vulnerabilities
+# https://crates.io/crates/cargo-audit
+# command cargo audit
