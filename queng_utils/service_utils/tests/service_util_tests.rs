914171975c7069638fd4553c4e9093432b88827a
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-25 12:36:58 +0800

    Updated ServiceUtil test
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_utils/service_utils/tests/service_util_tests.rs
+++ queng_utils/service_utils/tests/service_util_tests.rs
@@ -1,17 +1,24 @@
+use std::env;
 use std::time::Duration;
 
 use common_config::prelude::ServiceID;
 use service_utils::ServiceUtil;
 
+fn setup_env() {
+    // Set the environment variable.
+    env::set_var("ENV", "LOCAL");
+}
 #[test]
 fn test_start_service_util() {
+    setup_env();
+
     let service_id = ServiceID::KaikoProxy;
     let svc_util = ServiceUtil::with_debug();
 
     let result = svc_util.start_service(&service_id);
     assert!(result.is_ok());
 
-    std::thread::sleep(Duration::from_secs(5));
+    std::thread::sleep(Duration::from_secs(25));
 
     let result = svc_util.stop_service(&service_id);
     assert!(result.is_ok());
