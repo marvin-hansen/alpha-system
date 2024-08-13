f840da5e669601616ec12f6a0b7fcd65085e3ec4
Marvin Hansen <marvin.hansen@gmail.com>
2024-08-13 13:28:22 +0800

    Code formtting
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_infra/postgres/pg_smdb/src/model/protocol_type/mod.rs
+++ queng_infra/postgres/pg_smdb/src/model/protocol_type/mod.rs
@@ -1,11 +1,14 @@
-use diesel::deserialize;
-use diesel::deserialize::{FromSql, FromSqlRow};
+use diesel::deserialize::FromSqlRow;
 use diesel::expression::AsExpression;
-use diesel::pg::{Pg, PgValue};
-use diesel::serialize;
-use diesel::serialize::{IsNull, Output, ToSql};
 use diesel::sql_types::SqlType;
-use std::io::Write;
+
+mod protocol_type_impl;
+
+//  Diesel type mapping requires a struct to derive a SqlType for custom ToSql and FromSql implementations
+#[derive(SqlType)]
+#[diesel(sql_type = protocol_type)]
+#[diesel(postgres_type(name = "protocol_type"))]
+pub struct PgProtocolType;
 
 #[derive(Debug, Clone, FromSqlRow, AsExpression, PartialEq, Eq)]
 #[diesel(sql_type = PgProtocolType)]
@@ -14,34 +17,4 @@ pub enum ProtocolType {
     GRPC,
     HTTP,
     UDP,
-}
-
-//  Diesel type mapping requires a struct to derive a SqlType for custom ToSql and FromSql implementations
-#[derive(SqlType)]
-#[diesel(sql_type = protocol_type)]
-#[diesel(postgres_type(name = "protocol_type"))]
-pub struct PgProtocolType;
-
-impl ToSql<PgProtocolType, Pg> for ProtocolType {
-    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> serialize::Result {
-        match *self {
-            ProtocolType::UnknownProtocol => out.write_all(b"UnknownProtocol")?,
-            ProtocolType::GRPC => out.write_all(b"GRPC")?,
-            ProtocolType::HTTP => out.write_all(b"HTTP")?,
-            ProtocolType::UDP => out.write_all(b"UDP")?,
-        }
-        Ok(IsNull::No)
-    }
-}
-
-impl FromSql<PgProtocolType, Pg> for ProtocolType {
-    fn from_sql(bytes: PgValue<'_>) -> deserialize::Result<Self> {
-        match bytes.as_bytes() {
-            b"UnknownProtocol" => Ok(ProtocolType::UnknownProtocol),
-            b"GRPC" => Ok(ProtocolType::GRPC),
-            b"HTTP" => Ok(ProtocolType::HTTP),
-            b"UDP" => Ok(ProtocolType::UDP),
-            _ => Ok(ProtocolType::UnknownProtocol),
-        }
-    }
 }
---
+++ queng_infra/postgres/pg_smdb/src/model/protocol_type/protocol_type_impl.rs
@@ -0,0 +1,30 @@
+use crate::model::protocol_type::{PgProtocolType, ProtocolType};
+use diesel::deserialize::FromSql;
+use diesel::pg::{Pg, PgValue};
+use diesel::serialize::{IsNull, Output, ToSql};
+use diesel::{deserialize, serialize};
+use std::io::Write;
+
+impl ToSql<PgProtocolType, Pg> for ProtocolType {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> serialize::Result {
+        match *self {
+            ProtocolType::UnknownProtocol => out.write_all(b"UnknownProtocol")?,
+            ProtocolType::GRPC => out.write_all(b"GRPC")?,
+            ProtocolType::HTTP => out.write_all(b"HTTP")?,
+            ProtocolType::UDP => out.write_all(b"UDP")?,
+        }
+        Ok(IsNull::No)
+    }
+}
+
+impl FromSql<PgProtocolType, Pg> for ProtocolType {
+    fn from_sql(bytes: PgValue<'_>) -> deserialize::Result<Self> {
+        match bytes.as_bytes() {
+            b"UnknownProtocol" => Ok(ProtocolType::UnknownProtocol),
+            b"GRPC" => Ok(ProtocolType::GRPC),
+            b"HTTP" => Ok(ProtocolType::HTTP),
+            b"UDP" => Ok(ProtocolType::UDP),
+            _ => Ok(ProtocolType::UnknownProtocol),
+        }
+    }
+}
