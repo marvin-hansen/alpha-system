110c1cfd8067e74fab335f7398a158d8c60af0f4
Marvin Hansen <marvin.hansen@gmail.com>
2024-08-22 09:40:04 +0800

    Reworked pg smdb
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_infra/postgres/pg_smdb/tests/service_tests.rs
+++ queng_infra/postgres/pg_smdb/tests/service_tests.rs
@@ -2,18 +2,12 @@ use common_config::prelude::ServiceID::SMDB;
 use common_config::prelude::{ServiceConfig, ServiceID};
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
-use dotenv::dotenv;
 use pg_smdb::model::service;
 use pg_smdb::model::service::UpdateService;
 use pg_smdb::run_smdb_db_migration;
-use std::env;
 
 fn postgres_connection_pool() -> Pool<ConnectionManager<PgConnection>> {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL")
-        .or_else(|_| env::var("POSTGRES_DATABASE_URL"))
-        .expect("DATABASE_URL must be set");
+    let database_url = "postgres://postgres:postgres@localhost/postgres";
 
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     Pool::builder()
