5d1ae0ccee506b7cf2477b419436b1a46c54d385
Marvin Hansen <marvin.hansen@gmail.com>
2024-08-28 17:51:51 +0800

    Trigger CI
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_infra/postgres/postgres_test_utils/tests/test_full_setup.rs
+++ queng_infra/postgres/postgres_test_utils/tests/test_full_setup.rs
@@ -3,6 +3,5 @@ use postgres_test_utils::prelude::*;
 #[tokio::test]
 async fn test_full_setup() {
     let result = postgres_full_setup(DB_TEST_URL).await;
-    //dbg!(&result);
     assert!(result.is_ok());
 }
