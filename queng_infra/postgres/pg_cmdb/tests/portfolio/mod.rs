0b0e28fa72775f0a74d1d25cf3246a8ae5fd4d84
Marvin Hansen <marvin.hansen@gmail.com>
2024-08-30 11:27:33 +0800

    Reverted CMDB tests to run on CI
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_infra/postgres/pg_cmdb/tests/portfolio/mod.rs
+++ queng_infra/postgres/pg_cmdb/tests/portfolio/mod.rs
@@ -1,16 +1,2 @@
 #[cfg(test)]
-mod check_if_exists_tests;
-#[cfg(test)]
-mod count_portfolio_tests;
-#[cfg(test)]
-mod create_portfolio_tests;
-#[cfg(test)]
-mod delete_portfolio_tests;
-#[cfg(test)]
-mod read_all_portfolios_tests;
-mod read_all_portfolios_tests_error;
-#[cfg(test)]
-mod read_portfolio_tests;
-mod update_portfolio_error_tests;
-#[cfg(test)]
-mod update_portfolio_tests;
+mod portfolio_tests;
