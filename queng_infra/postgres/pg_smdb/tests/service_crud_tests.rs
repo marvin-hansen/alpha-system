5ee8dd107523d2a063f9321c887e602247e6e6b7
Marvin Hansen <marvin.hansen@gmail.com>
2024-09-01 13:33:53 +0800

    Trigger CI
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_infra/postgres/pg_smdb/tests/service_crud_tests.rs
+++ queng_infra/postgres/pg_smdb/tests/service_crud_tests.rs
@@ -1,6 +1,14 @@
+use common_database::prelude::PostgresDBSchema;
 use diesel::Connection;
 use pg_smdb::model::service;
 use postgres_test_utils::prelude::*;
+
+#[tokio::test]
+async fn test_smdb_setup() {
+    let result = postgres_schema_setup(PostgresDBSchema::PostgresDBSchemaSMDB, DB_TEST_URL).await;
+    // dbg!(&result);
+    assert!(result.is_ok());
+}
 
 #[tokio::test]
 async fn test_create_instrument() {
