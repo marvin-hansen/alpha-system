ec920b7010950007350c64b990dffaeee0394881
Marvin Hansen <marvin.hansen@gmail.com>
2024-09-05 16:43:23 +0800

    Added documentation to instrument implementation on pg metadb
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_infra/postgres/pg_metadb/src/model/instrument/instrument_impl.rs
+++ queng_infra/postgres/pg_metadb/src/model/instrument/instrument_impl.rs
@@ -5,6 +5,17 @@ use common_metadata::prelude::MetaInstrument;
 use diesel::{ExpressionMethods, OptionalExtension, QueryDsl, QueryResult, RunQueryDsl};
 
 impl Instrument {
+    /// Creates a new instrument entry in the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `conn` - A mutable reference to the database connection.
+    /// * `meta_instrument` - The metadata of the instrument to be inserted.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the inserted `MetaInstrument` if successful, else an `Error`.
+    ///
     pub fn create(
         conn: &mut Connection,
         meta_instrument: MetaInstrument,
@@ -19,6 +30,17 @@ impl Instrument {
         }
     }
 
+    /// Inserts a collection of instruments into the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `conn` - A mutable reference to the database connection.
+    /// * `meta_instruments` - A vector of `MetaInstrument` instances to be inserted.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating the success of the operation.
+    ///
     pub fn create_instrument_collection(
         conn: &mut Connection,
         meta_instruments: Vec<MetaInstrument>,
@@ -33,6 +55,16 @@ impl Instrument {
             .map(|_| true)
     }
 
+    /// Counts the number of instruments in the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `conn` - A mutable reference to the database connection.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<u64, Error>` - The total count of instruments if successful, an error otherwise.
+    ///
     pub fn count(conn: &mut Connection) -> Result<u64, diesel::result::Error> {
         instruments_table
             .count()
@@ -40,6 +72,17 @@ impl Instrument {
             .map(|c| c as u64)
     }
 
+    /// Checks if an instrument with the given ID exists in the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `conn` - A mutable reference to the database connection.
+    /// * `instrument_id` - The ID of the instrument to check.
+    ///
+    /// # Returns
+    ///
+    /// * `QueryResult<bool>` - A result indicating whether the instrument exists (true) or not (false).
+    ///
     pub fn check_if_instrument_id_exists(
         conn: &mut Connection,
         instrument_id: &str,
@@ -52,6 +95,16 @@ impl Instrument {
         Ok(exists)
     }
 
+    /// Reads a `MetaInstrument` from the database based on the provided instrument ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `conn` - A mutable reference to the database connection.
+    /// * `instrument_id` - The ID of the instrument to read.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the retrieved `MetaInstrument` if successful, else an `Error`.
     pub fn read(
         conn: &mut Connection,
         instrument_id: &str,
@@ -62,6 +115,15 @@ impl Instrument {
             .map(|instrument| instrument.to_meta_instrument())
     }
 
+    /// Reads all instruments from the database and converts them to `MetaInstrument` objects.
+    ///
+    /// # Arguments
+    ///
+    /// * `conn` - A mutable reference to the database connection.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing a vector of `MetaInstrument` objects if successful, else a `diesel::result::Error`.
     pub fn read_all(conn: &mut Connection) -> Result<Vec<MetaInstrument>, diesel::result::Error> {
         instruments_table
             .load::<Instrument>(conn)
@@ -73,6 +135,17 @@ impl Instrument {
             })
     }
 
+    /// Updates an instrument entry in the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `conn` - A mutable reference to the database connection.
+    /// * `instrument_id` - The ID of the instrument to be updated.
+    /// * `meta_instrument` - The updated metadata of the instrument.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating the number of rows affected if successful, else an `Error`.
     pub fn update(
         conn: &mut Connection,
         instrument_id: &str,
@@ -87,6 +160,20 @@ impl Instrument {
         .execute(conn)
     }
 
+    /// Deletes an instrument from the database by instrument ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `conn` - A mutable reference to the database connection.
+    /// * `instrument_id` - The ID of the instrument to delete.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` containing the number of rows affected by the delete operation.
+    /// If the instrument does not exist, the query will return `Ok(0)`.
+    /// If the instrument exists and was deleted, the query will return `Ok(1)`.
+    ///
+    /// Note, delete only returns an error when either the database connection or the query fails.
     pub fn delete(
         conn: &mut Connection,
         instrument_id: &str,
