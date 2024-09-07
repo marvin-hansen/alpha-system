64269e96cabb4200c6d6f369085bcbe995814ffb
Marvin Hansen <marvin.hansen@gmail.com>
2024-09-07 18:21:07 +0800

    Added more tests to queng_commong
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_common/common_data_bar/tests/mod.rs
+++ queng_common/common_data_bar/tests/mod.rs
@@ -1,5 +1,13 @@
 #[cfg(test)]
+mod ohlcv_bar_tests;
+
+#[cfg(test)]
 mod time_resolution_tests;
 
 #[cfg(test)]
-mod ohlcv_bar_tests;
+mod trade_bar_tests;
+
+#[cfg(test)]
+mod data_type_tests;
+#[cfg(test)]
+mod sampled_bars_tests;
--- queng_common/common_metadata/tests/exchanges_tests.rs
+++ queng_common/common_metadata/tests/exchanges_tests.rs
@@ -23,3 +23,36 @@ fn test_exchanges_root_and_exchange_properties() {
     assert_eq!(test_exchange.name, "Test Exchange");
     assert_eq!(test_exchange.kaiko_legacy_slug, "test-exchange");
 }
+
+#[test]
+fn test_exchange_code() {
+    let exchange = MetaExchange {
+        code: "TEST".to_string(),
+        name: "Test Exchange".to_string(),
+        kaiko_legacy_slug: "test-exchange".to_string(),
+    };
+
+    assert_eq!(exchange.code, "TEST");
+}
+
+#[test]
+fn test_exchange_name() {
+    let exchange = MetaExchange {
+        code: "TEST".to_string(),
+        name: "Test Exchange".to_string(),
+        kaiko_legacy_slug: "test-exchange".to_string(),
+    };
+
+    assert_eq!(exchange.name, "Test Exchange");
+}
+
+#[test]
+fn test_exchange_kaiko_legacy_slug() {
+    let exchange = MetaExchange {
+        code: "TEST".to_string(),
+        name: "Test Exchange".to_string(),
+        kaiko_legacy_slug: "test-exchange".to_string(),
+    };
+
+    assert_eq!(exchange.kaiko_legacy_slug, "test-exchange");
+}
