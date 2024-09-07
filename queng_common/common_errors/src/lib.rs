8e918acee4115b9202818fd3ec2fe7e40b4d69a2
Marvin Hansen <marvin.hansen@gmail.com>
2024-09-07 21:23:45 +0800

    Added tests to common_errors
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_common/common_errors/src/lib.rs
+++ queng_common/common_errors/src/lib.rs
@@ -1,6 +1,6 @@
 pub(crate) mod init_error;
 pub(crate) mod lookup_error;
-pub(crate) mod message_client_error;
+pub(crate) mod message_client_config_error;
 pub(crate) mod message_processing_error;
 mod postgres_error;
 pub mod prelude;
--- queng_common/common_errors/tests/mod.rs
+++ queng_common/common_errors/tests/mod.rs
@@ -1,6 +1,10 @@
 #[cfg(test)]
 mod init_error_tests;
 #[cfg(test)]
+mod lookup_error_tests;
+#[cfg(test)]
 mod message_client_config_error_tests;
 #[cfg(test)]
 mod message_processing_error_tests;
+#[cfg(test)]
+mod validation_error_tests;
