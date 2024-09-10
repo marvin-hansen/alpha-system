031875347b1c5e9c527d67976fbe8d0c508a7061
Marvin Hansen <marvin.hansen@gmail.com>
2024-09-10 22:22:30 +0800

    Added first test to PostgresCMDBManager
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_infra/postgres/pg_cmdb_manager/tests/db_prtf_tests.rs
+++ queng_infra/postgres/pg_cmdb_manager/tests/db_prtf_tests.rs
@@ -1,1 +1,13 @@
+use container_specs_postgres::postgres_db_container_config;
+use docker_utils::prelude::DockerUtil;
 
+#[tokio::test]
+async fn all_setup() {
+    let env = DockerUtil::with_debug().expect("Failed to get DockerUtil");
+
+    // Start or reuse a test postgres container
+    let container_config = postgres_db_container_config();
+    let result = env.get_or_start_container_config(&container_config);
+    // dbg!(&result);
+    assert!(result.is_ok());
+}
