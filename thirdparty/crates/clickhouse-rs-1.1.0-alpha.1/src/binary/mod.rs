b64bc68b6605cb8cc4efda4f213fd38fed9c0191
Marvin Hansen <marvin.hansen@gmail.com>
2024-09-15 08:42:50 +0800

    Trigger CI
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- thirdparty/crates/clickhouse-rs-1.1.0-alpha.1/src/binary/mod.rs
+++
@@ -1,7 +0,0 @@
-pub(crate) use self::{encoder::Encoder, parser::Parser, read_ex::ReadEx, uvarint::put_uvarint};
-
-mod encoder;
-mod parser;
-pub mod protocol;
-mod read_ex;
-mod uvarint;
--- thirdparty/crates/klickhouse-0.13.0/build.rs
+++
@@ -1,7 +0,0 @@
-use rustc_version::{version, Version};
-
-fn main() {
-    if version().unwrap() >= Version::parse("1.51.0").unwrap() {
-        println!("cargo:rustc-cfg=const_generics");
-    }
-}
