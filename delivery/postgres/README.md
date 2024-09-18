900a8fae8b517f798bea65af710e1ce289e31ed1
Marvin Hansen <marvin.hansen@gmail.com>
2024-09-18 10:53:22 +0800

    Added initial postgres cluster config.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ delivery/postgres/README.md
@@ -0,0 +1,41 @@
+# Install
+
+1) Install operator
+
+```shell
+kubectl apply --server-side -f cnpg-1.24.0.yaml
+```
+
+2) Verify oprerator
+
+```shell
+kubectl get deployment -n cnpg-system cnpg-controller-manager
+```
+
+!!! Warning If you are deploying CloudNativePG on GKE and get an error (... failed to call webhook...), 
+be aware that by default traffic between worker nodes and control plane is blocked by the firewall except for a few specific ports, as explained in the official docs and by this issue. You'll need to either change the targetPort in the webhook service, to be one of the allowed ones, or open the webhooks' port (9443) on the firewall.
+
+https://github.com/cloudnative-pg/cloudnative-pg/blob/main/docs/src/installation_upgrade.md
+
+
+3) Install postgres cluster
+
+```shell
+kubectl apply -f cluster.yaml
+```
+
+4) Verify postgres cluster
+
+```shell
+kubectl get pods 
+```
+
+or
+```shell
+kubectl get pods -l cnpg.io/cluster=postgres-cluster
+```
+
+SRC:
+https://github.com/cloudnative-pg/cloudnative-pg/blob/main/docs/src/quickstart.md
+
+https://github.com/cloudnative-pg/cloudnative-pg/blob/main/docs/src/samples/cluster-example-secret.yaml
---
+++ delivery/postgres/create_user_secrets.sh
@@ -0,0 +1,85 @@
+#
+# Copyright (c) 2021. Marvin Friedrich Lars Hansen. All Rights Reserved. Contact: marvin.hansen@gmail.com
+#
+
+# bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ echo "* Delete previously generated credentials"
+ command kubectl delete secret post-init-sql-secret
+ command kubectl delete secret cmdb-creds
+ command kubectl delete secret imdb-creds
+ command kubectl delete secret mddb-creds
+ command kubectl delete secret smdb-creds
+
+echo "* Generating user name and password"
+CMDB_USER='cmdbuser'
+CMDB_PASSWORD=$(openssl rand -base64 32)
+
+IMDB_USER='imdbuser'
+IMDB_PASSWORD=$(openssl rand -base64 32)
+
+MDDB_USER='mddbuser'
+MDDB_PASSWORD=$(openssl rand -base64 32)
+
+SMDB_USER='smdbuser'
+SMDB_PASSWORD=$(openssl rand -base64 32)
+
+echo "* Create secret to store user and password"
+command kubectl create secret generic cmdb-creds --from-literal=CMDB_USER="$CMDB_USER" --from-literal=CMDB_PASSWORD="$CMDB_PASSWORD"
+command kubectl create secret generic imdb-creds --from-literal=IMDB_USER="$IMDB_USER" --from-literal=IMDB_PASSWORD="$IMDB_PASSWORD"
+command kubectl create secret generic mddb-creds --from-literal=MDDDB_USER="$MDDB_USER" --from-literal=MDDDB_PASSWORD="$MDDB_PASSWORD"
+command kubectl create secret generic smdb-creds --from-literal=SMDB_USER="$SMDB_USER" --from-literal=SMDB_PASSWORD="$SMDB_PASSWORD"
+
+SQL_SECRET="---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: post-init-sql-secret
+  namespace: cnpg-system
+stringData:
+  secret.sql: |
+    CREATE USER $CMDB_USER with encrypted password '$CMDB_PASSWORD';
+    CREATE USER $IMDB_USER with encrypted password '$IMDB_PASSWORD';
+    CREATE USER $MDDB_USER with encrypted password '$MDDB_PASSWORD';
+    CREATE USER $SMDB_USER with encrypted password '$SMDB_PASSWORD';
+
+    CREATE SCHEMA IF NOT EXISTS cmdb AUTHORIZATION $CMDB_USER;
+    CREATE SCHEMA IF NOT EXISTS imdb AUTHORIZATION $IMDB_USER;
+    CREATE SCHEMA IF NOT EXISTS mddb AUTHORIZATION $MDDB_USER;
+    CREATE SCHEMA IF NOT EXISTS smdb AUTHORIZATION $SMDB_USER;
+---"
+
+# https://stackoverflow.com/questions/68535929/run-kubectl-apply-using-yaml-stored-in-a-variable
+echo "$SQL_SECRET" | kubectl apply -f -
+
+# For access to the secrets from a client authenticated to the cluster:
+# command kubectl get secret --namespace default cmdb-creds -o jsonpath='{.data.CMDB_USER}' | base64 --decode -
+# command kubectl get secret --namespace default cmdb-creds -o jsonpath='{.data.CMDB_PASSWORD}' | base64 --decode -
+
+# Clear all generated credentials
+SQL_SECRET=''
+CMDB_USER=''
+CMDB_PASSWORD=''
+IMDB_USER=''
+IMDB_PASSWORD=''
+MDDB_USER=''
+MDDB_PASSWORD=''
+SMDB_USER=''
+SMDB_PASSWORD=''
+
+echo "Credentials generated and stored in kubernetes secrets"
+
+# An Introduction to Kubernetes Secrets and ConfigMaps
+# https://opensource.com/article/19/6/introduction-kubernetes-secrets-and-configmaps
+
+# Kubernetes Fundamentals, Part 3: How to Use Kubernetes Secrets
+# https://blog.newrelic.com/engineering/how-to-use-kubernetes-secrets/
+
+# Define an environment variable for a container
+# https://kubernetes.io/docs/tasks/inject-data-application/define-environment-variable-container/
+
+# Using Kubernetes Secrets as Environment Variables
+# https://medium.com/faun/using-kubernetes-secrets-as-environment-variables-5ea3ef7581ef
