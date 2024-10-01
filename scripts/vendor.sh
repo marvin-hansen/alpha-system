404975dc708a0c658fb2c8ed87ab50d3646b313d
Marvin Hansen <marvin.hansen@gmail.com>
2024-10-01 16:57:35 +0800

    Updated vendor script.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -3,4 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Updates base image to latest version
+command bazel run @rules_apko//apko lock images/base_image/apko.yaml
+
+# Updates all vendored crates
 command bazel run //thirdparty:crates_vendor
