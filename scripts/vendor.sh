8e2548bb958358b697fb4e5a5004c956081f1986
Marvin Hansen <marvin.hansen@gmail.com>
2024-10-08 17:11:35 +0800

    Updated vendored deps.

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -3,8 +3,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Updates base image to latest version
-command bazel run @rules_apko//apko lock images/base_image/apko.yaml
-
 # Updates all vendored crates
 command bazel run //thirdparty:crates_vendor
