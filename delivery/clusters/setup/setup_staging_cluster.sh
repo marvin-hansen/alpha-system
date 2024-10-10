83c8d9441dec927932461785a32f689baa41eca7
Marvin Hansen <marvin.hansen@gmail.com>
2024-10-10 21:22:29 +0800

    Updated CD config
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- delivery/clusters/setup/setup_staging_cluster.sh
+++ delivery/clusters/setup/setup_staging_cluster.sh
@@ -24,46 +24,9 @@ echo "► Configuring cluster DNS completed"
 
 echo ""
 echo "==============================="
-echo " Configure cluster secrets:  "
+echo " Configure Docker Registry:  "
 echo "==============================="
 echo ""
-
-echo "► Configuring cluster secrets"
-
-# Create temporary file
-command cp secret-template.yaml secret.yaml
-
-# User name only consists of letters, no numbers and special characters to avoid any problems.
-USER="dbgwuser"
-# Generate random password
-PASSWORD=$(openssl rand -hex 16)
-
-SECRETNAME="postgres-app"
-YAML_FILE="secret.yaml"
-
-# Replace TOPSECRET with the value from PASSWORD
-sed -i '' "s/TOPSECRET/$PASSWORD/g" "$YAML_FILE"
-
-# Replace USERNAME with the value from USER
-command sed -i '' "s/USERNAME/$USER/g" "$YAML_FILE"
-
-# Replace SECRETID with the value from SECRETNAME
-command sed -i '' "s/SECRETID/$SECRETNAME/g" "$YAML_FILE"
-
-# Install secret in cluster
-command kubectl apply -f secret.yaml
-
-# Remove temporary file
-command rm secret.yaml
-
-# store DB user & password in kubernetes secret accessed only by the dbgw container.
-# See manifests/deployment.yml for ENV variables that are accessible from within the container.
-command kubectl create secret generic postgres-auth \
-  --from-literal=username="$USER" \
-  --from-literal=password="$PASSWORD"
-
-# database must match DB name in delivery/infra/base/pg-cluster.yaml
-command kubectl create secret generic postgres-db --from-literal=database="quantum"
 
 kubectl create secret docker-registry artifact-registry --docker-server=asia-northeast1-docker.pkg.dev/future-309012/image-repo --docker-username=_json_key --docker-password="$(cat future.json)" --dry-run=client -o yaml > artifact-registry.yaml
 
@@ -71,11 +34,8 @@ kubectl apply -f artifact-registry.yaml
 
 command rm artifact-registry.yaml
 
-# Null variable
-USER=""
-PASSWORD=""
 
-echo "► Configuring cluster secrets completed"
+echo "► Configuring docker registry completed"
 
 echo ""
 echo "==============================="
