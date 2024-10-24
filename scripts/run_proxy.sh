f31ce5097421b400961a5df139f1799aeabf8e8d
Marvin Hansen <marvin.hansen@gmail.com>
2024-10-24 19:44:17 +0800

    Updated make proxy script
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/run_proxy.sh
+++ scripts/run_proxy.sh
@@ -4,7 +4,8 @@ set -o nounset
 set -o pipefail
 
 # Run with Bazel
-# ENV=LOCAL bazel run  -c opt //alias:kaiko_proxy
+ENV=LOCAL bazel run  -c opt //alias:kaiko_proxy
 
+# Docker network is about 20x slower than localhost when accessing the proxy locally.
 # Find image tag and URL in queng_specs/container_specs/container_specs_kaiko/src/lib.rs
-command docker run --name apiproxy-7777 -p 7777:7777 -e ENV=LOCAL -d index.docker.io/hansenmarvin/api_proxy:280562f
+#command docker run --name apiproxy-7777 -p 7777:7777 -e ENV=LOCAL -d asia-northeast1-docker.pkg.dev/future-309012/image-repo/kaiko_proxy:96fd9937-1729768888
