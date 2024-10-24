13b6ffb67db44d45ef0789be2f6a4534aaca2682
Marvin Hansen <marvin.hansen@gmail.com>
2024-10-24 19:28:33 +0800

    Updated import script
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/import.sh
+++ scripts/import.sh
@@ -3,4 +3,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-ENV=LOCAL bazel run //queng_import/postgres/pg_import_services
+ENV=LOCAL bazel run //alias:pg_import_services
+
+ENV=LOCAL bazel run //alias:pg_import_metadata
