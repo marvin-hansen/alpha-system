c8d8d710e30a8a11e84b593527bb682d6824f22a
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-01 15:01:48 +0800

    Added mddb exchange utils
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_proto/proto_mddb_utils/src/mddb_exchanges_utils.rs
+++ queng_proto/proto_mddb_utils/src/mddb_exchanges_utils.rs
@@ -1,1 +1,83 @@
+use common_metadata::prelude::MetaExchange;
 
+use proto_mddb::proto::{
+    CheckIfExchangeIdExistsRequest, CheckIfExchangeIdExistsResponse, CountExchangesResponse,
+    GetAllExchangesRequest, GetExchangeRequest, LookupExchangeNameRequest,
+};
+
+// Request
+pub fn get_check_if_exchange_exists_request(exchange_code: &str) -> CheckIfExchangeIdExistsRequest {
+    CheckIfExchangeIdExistsRequest {
+        exchange_code: exchange_code.to_string(),
+    }
+}
+
+pub fn get_exchange_request(exchange_code: &str) -> GetExchangeRequest {
+    GetExchangeRequest {
+        exchange_code: exchange_code.to_string(),
+    }
+}
+
+pub fn get_all_exchanges_request() -> GetAllExchangesRequest {
+    GetAllExchangesRequest {}
+}
+pub fn get_lookup_exchange_name_request(exchange_code: &str) -> LookupExchangeNameRequest {
+    LookupExchangeNameRequest {
+        exchange_code: exchange_code.to_string(),
+    }
+}
+
+// Response
+
+pub fn get_count_exchanges_response(count: u64) -> CountExchangesResponse {
+    CountExchangesResponse { count }
+}
+
+pub fn get_check_if_exchange_exists_response(
+    exchange_code: &str,
+    exists: bool,
+) -> CheckIfExchangeIdExistsResponse {
+    CheckIfExchangeIdExistsResponse {
+        exchange_code: exchange_code.to_string(),
+        exists,
+    }
+}
+
+pub fn get_exchange_response(
+    meta_exchange: &MetaExchange,
+) -> proto_mddb::proto::GetExchangeResponse {
+    proto_mddb::proto::GetExchangeResponse {
+        exchange: Option::from(meta_exchange_to_proto_exchange(meta_exchange)),
+    }
+}
+
+pub fn get_all_exchanges_response(
+    meta_exchanges: Vec<MetaExchange>,
+) -> proto_mddb::proto::GetAllExchangesResponse {
+    proto_mddb::proto::GetAllExchangesResponse {
+        exchanges: meta_exchanges
+            .into_iter()
+            .map(|meta_exchange: MetaExchange| meta_exchange_to_proto_exchange(&meta_exchange))
+            .collect(),
+    }
+}
+
+pub fn meta_exchange_to_proto_exchange(
+    meta_exchange: &MetaExchange,
+) -> proto_mddb::proto::ProtoMetaExchange {
+    proto_mddb::proto::ProtoMetaExchange {
+        exchange_code: meta_exchange.code.to_string(),
+        exchange_name: meta_exchange.name.to_string(),
+        exchange_hash: meta_exchange.hash(),
+    }
+}
+
+pub fn proto_exchange_to_meta_exchange(
+    proto_exchange: &proto_mddb::proto::ProtoMetaExchange,
+) -> MetaExchange {
+    MetaExchange {
+        code: proto_exchange.exchange_code.to_string(),
+        name: proto_exchange.exchange_name.to_string(),
+        kaiko_legacy_slug: "".to_string(),
+    }
+}
--- queng_proto/proto_mddb_utils/tests/mddb_exchanges_utils_tests.rs
+++ queng_proto/proto_mddb_utils/tests/mddb_exchanges_utils_tests.rs
@@ -1,1 +1,119 @@
+use proto_mddb_utils::mddb_exchanges_utils::{
+    get_all_exchanges_request, get_all_exchanges_response, get_check_if_exchange_exists_request,
+    get_check_if_exchange_exists_response, get_count_exchanges_response, get_exchange_request,
+    get_exchange_response, get_lookup_exchange_name_request, meta_exchange_to_proto_exchange,
+    proto_exchange_to_meta_exchange,
+};
 
+use common_metadata::prelude::MetaExchange;
+use proto_mddb::proto::ProtoMetaExchange;
+
+#[test]
+fn test_get_check_if_exchange_exists_request() {
+    let exchange_code = "binance";
+    let request = get_check_if_exchange_exists_request(exchange_code);
+    assert_eq!(request.exchange_code, exchange_code);
+}
+
+#[test]
+fn test_get_exchange_request() {
+    let exchange_code = "kraken";
+    let request = get_exchange_request(exchange_code);
+    assert_eq!(request.exchange_code, exchange_code);
+}
+
+#[test]
+fn test_get_all_exchanges_request() {
+    let request = get_all_exchanges_request();
+    assert_eq!(format!("{:?}", request), "GetAllExchangesRequest");
+}
+
+#[test]
+fn test_get_lookup_exchange_name_request() {
+    let exchange_code = "coinbase";
+    let request = get_lookup_exchange_name_request(exchange_code);
+    assert_eq!(request.exchange_code, exchange_code);
+}
+
+#[test]
+fn test_get_count_exchanges_response() {
+    let count = 42;
+    let response = get_count_exchanges_response(count);
+    assert_eq!(response.count, count);
+}
+
+#[test]
+fn test_get_check_if_exchange_exists_response() {
+    let exchange_code = "gemini";
+    let exists = true;
+    let response = get_check_if_exchange_exists_response(exchange_code, exists);
+    assert_eq!(response.exchange_code, exchange_code);
+    assert_eq!(response.exists, exists);
+}
+
+#[test]
+fn test_get_exchange_response() {
+    let meta_exchange = MetaExchange {
+        code: "bitfinex".to_string(),
+        name: "Bitfinex".to_string(),
+        kaiko_legacy_slug: "".to_string(),
+    };
+    let response = get_exchange_response(&meta_exchange);
+    let proto_exchange = response.exchange.unwrap();
+    assert_eq!(proto_exchange.exchange_code, meta_exchange.code);
+    assert_eq!(proto_exchange.exchange_name, meta_exchange.name);
+    assert_eq!(proto_exchange.exchange_hash, meta_exchange.hash());
+}
+
+#[test]
+fn test_get_all_exchanges_response() {
+    let meta_exchanges = vec![
+        MetaExchange {
+            code: "huobi".to_string(),
+            name: "Huobi".to_string(),
+            kaiko_legacy_slug: "".to_string(),
+        },
+        MetaExchange {
+            code: "okex".to_string(),
+            name: "OKEx".to_string(),
+            kaiko_legacy_slug: "".to_string(),
+        },
+    ];
+
+    let response = get_all_exchanges_response(meta_exchanges.clone());
+    assert_eq!(response.exchanges.len(), 2);
+
+    for (proto_exchange, meta_exchange) in response.exchanges.iter().zip(meta_exchanges.iter()) {
+        assert_eq!(proto_exchange.exchange_code, meta_exchange.code);
+        assert_eq!(proto_exchange.exchange_name, meta_exchange.name);
+        assert_eq!(proto_exchange.exchange_hash, meta_exchange.hash());
+    }
+}
+
+#[test]
+fn test_meta_exchange_to_proto_exchange() {
+    let meta_exchange = MetaExchange {
+        code: "kucoin".to_string(),
+        name: "KuCoin".to_string(),
+        kaiko_legacy_slug: "".to_string(),
+    };
+
+    let proto_exchange = meta_exchange_to_proto_exchange(&meta_exchange);
+    assert_eq!(proto_exchange.exchange_code, meta_exchange.code);
+    assert_eq!(proto_exchange.exchange_name, meta_exchange.name);
+    assert_eq!(proto_exchange.exchange_hash, meta_exchange.hash());
+}
+
+#[test]
+fn test_proto_exchange_to_meta_exchange() {
+    let proto_exchange = ProtoMetaExchange {
+        exchange_code: "bitstamp".to_string(),
+        exchange_name: "Bitstamp".to_string(),
+        exchange_hash: "some_hash".to_string(),
+    };
+
+    let meta_exchange = proto_exchange_to_meta_exchange(&proto_exchange);
+    assert_eq!(meta_exchange.code, proto_exchange.exchange_code);
+    assert_eq!(meta_exchange.name, proto_exchange.exchange_name);
+    assert_eq!(meta_exchange.kaiko_legacy_slug, "");
+}
