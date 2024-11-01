240aa358593a7884454fa80d65c5d3040cf28333
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-01 15:44:24 +0800

    Added mddb instrument utils with tests
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_proto/proto_mddb_utils/src/mddb_instruments_utils.rs
+++ queng_proto/proto_mddb_utils/src/mddb_instruments_utils.rs
@@ -1,1 +1,334 @@
+use common_metadata::prelude::{InstrumentMetadata, MetaInstrument};
 
+// Request
+
+pub fn get_count_instruments_request() -> proto_mddb::proto::CountInstrumentsRequest {
+    proto_mddb::proto::CountInstrumentsRequest {}
+}
+
+pub fn get_check_if_instrument_exists_request(
+    instrument_id: &str,
+) -> proto_mddb::proto::CheckIfInstrumentIdExistsRequest {
+    proto_mddb::proto::CheckIfInstrumentIdExistsRequest {
+        instrument_id: instrument_id.to_string(),
+    }
+}
+
+pub fn get_instrument_by_id_request(
+    instrument_id: &str,
+) -> proto_mddb::proto::GetInstrumentByIdRequest {
+    proto_mddb::proto::GetInstrumentByIdRequest {
+        instrument_id: instrument_id.to_string(),
+    }
+}
+
+pub fn get_instrument_by_figi_request(
+    instrument_figi: &str,
+) -> proto_mddb::proto::GetInstrumentByFigiRequest {
+    proto_mddb::proto::GetInstrumentByFigiRequest {
+        instrument_figi: instrument_figi.to_string(),
+    }
+}
+
+pub fn get_all_instruments_request() -> proto_mddb::proto::GetAllInstrumentsRequest {
+    proto_mddb::proto::GetAllInstrumentsRequest {}
+}
+pub fn get_all_instruments_for_base_asset_request(
+    base_asset: &str,
+) -> proto_mddb::proto::GetAllInstrumentsForBaseAssetRequest {
+    proto_mddb::proto::GetAllInstrumentsForBaseAssetRequest {
+        base_asset: base_asset.to_string(),
+    }
+}
+
+pub fn get_all_instruments_for_quote_asset_request(
+    quote_asset: &str,
+) -> proto_mddb::proto::GetAllInstrumentsForQuoteAssetRequest {
+    proto_mddb::proto::GetAllInstrumentsForQuoteAssetRequest {
+        quote_asset: quote_asset.to_string(),
+    }
+}
+
+pub fn get_all_instruments_for_exchange_request(
+    exchange_code: &str,
+) -> proto_mddb::proto::GetAllInstrumentsForExchangeRequest {
+    proto_mddb::proto::GetAllInstrumentsForExchangeRequest {
+        exchange_code: exchange_code.to_string(),
+    }
+}
+
+pub fn get_all_instruments_for_base_asset_and_exchange_request(
+    exchange_code: &str,
+    base_asset: &str,
+) -> proto_mddb::proto::GetAllInstrumentsForBaseAssetAndExchangeRequest {
+    proto_mddb::proto::GetAllInstrumentsForBaseAssetAndExchangeRequest {
+        exchange_code: exchange_code.to_string(),
+        base_asset: base_asset.to_string(),
+    }
+}
+
+pub fn get_all_instruments_for_quote_asset_and_exchange_request(
+    exchange_code: &str,
+    quote_asset: &str,
+) -> proto_mddb::proto::GetAllInstrumentsForQuoteAssetAndExchangeRequest {
+    proto_mddb::proto::GetAllInstrumentsForQuoteAssetAndExchangeRequest {
+        exchange_code: exchange_code.to_string(),
+        quote_asset: quote_asset.to_string(),
+    }
+}
+
+pub fn get_all_instruments_for_base_quote_asset_and_exchange_request(
+    exchange_code: &str,
+    base_asset: &str,
+    quote_asset: &str,
+) -> proto_mddb::proto::GetAllInstrumentsForBaseQuoteAssetAndExchangeRequest {
+    proto_mddb::proto::GetAllInstrumentsForBaseQuoteAssetAndExchangeRequest {
+        exchange_code: exchange_code.to_string(),
+        base_asset: base_asset.to_string(),
+        quote_asset: quote_asset.to_string(),
+    }
+}
+
+pub fn get_lookup_instrument_exchange_pair_code_request(
+    instrument_id: &str,
+) -> proto_mddb::proto::LookupInstrumentExchangePairCodeRequest {
+    proto_mddb::proto::LookupInstrumentExchangePairCodeRequest {
+        instrument_id: instrument_id.to_string(),
+    }
+}
+
+pub fn get_lookup_instrument_figi_request(
+    instrument_id: &str,
+) -> proto_mddb::proto::LookupInstrumentFigiRequest {
+    proto_mddb::proto::LookupInstrumentFigiRequest {
+        instrument_id: instrument_id.to_string(),
+    }
+}
+
+// Response
+
+pub fn get_count_instruments_response(count: u64) -> proto_mddb::proto::CountInstrumentsResponse {
+    proto_mddb::proto::CountInstrumentsResponse { count }
+}
+
+pub fn get_check_if_instrument_exists_response(
+    instrument_id: &str,
+    exists: bool,
+) -> proto_mddb::proto::CheckIfInstrumentIdExistsResponse {
+    proto_mddb::proto::CheckIfInstrumentIdExistsResponse {
+        instrument_id: instrument_id.to_string(),
+        exists,
+    }
+}
+
+pub fn get_instrument_by_id_response(
+    meta_instrument: MetaInstrument,
+) -> proto_mddb::proto::GetInstrumentByIdResponse {
+    proto_mddb::proto::GetInstrumentByIdResponse {
+        instrument: Option::from(meta_instrument_to_proto_instrument(&meta_instrument)),
+    }
+}
+
+pub fn get_instrument_by_figi_response(
+    meta_instrument: MetaInstrument,
+) -> proto_mddb::proto::GetInstrumentByFigiResponse {
+    proto_mddb::proto::GetInstrumentByFigiResponse {
+        instrument: Option::from(meta_instrument_to_proto_instrument(&meta_instrument)),
+    }
+}
+
+pub fn get_all_instruments_response(
+    meta_instruments: Vec<MetaInstrument>,
+) -> proto_mddb::proto::GetAllInstrumentsResponse {
+    proto_mddb::proto::GetAllInstrumentsResponse {
+        instruments: meta_instruments
+            .into_iter()
+            .map(|meta_instrument: MetaInstrument| {
+                meta_instrument_to_proto_instrument(&meta_instrument)
+            })
+            .collect(),
+    }
+}
+
+pub fn get_all_instruments_for_base_asset_response(
+    meta_instruments: Vec<MetaInstrument>,
+) -> proto_mddb::proto::GetAllInstrumentsForBaseAssetResponse {
+    proto_mddb::proto::GetAllInstrumentsForBaseAssetResponse {
+        instruments: meta_instruments
+            .into_iter()
+            .map(|meta_instrument: MetaInstrument| {
+                meta_instrument_to_proto_instrument(&meta_instrument)
+            })
+            .collect(),
+    }
+}
+
+pub fn get_all_instruments_for_quote_asset_response(
+    meta_instruments: Vec<MetaInstrument>,
+) -> proto_mddb::proto::GetAllInstrumentsForQuoteAssetResponse {
+    proto_mddb::proto::GetAllInstrumentsForQuoteAssetResponse {
+        instruments: meta_instruments
+            .into_iter()
+            .map(|meta_instrument: MetaInstrument| {
+                meta_instrument_to_proto_instrument(&meta_instrument)
+            })
+            .collect(),
+    }
+}
+
+pub fn get_all_instruments_for_exchange_response(
+    meta_instruments: Vec<MetaInstrument>,
+) -> proto_mddb::proto::GetAllInstrumentsForExchangeResponse {
+    proto_mddb::proto::GetAllInstrumentsForExchangeResponse {
+        instruments: meta_instruments
+            .into_iter()
+            .map(|meta_instrument: MetaInstrument| {
+                meta_instrument_to_proto_instrument(&meta_instrument)
+            })
+            .collect(),
+    }
+}
+
+pub fn get_all_instruments_for_base_asset_and_exchange_response(
+    meta_instruments: Vec<MetaInstrument>,
+) -> proto_mddb::proto::GetAllInstrumentsForBaseAssetAndExchangeResponse {
+    proto_mddb::proto::GetAllInstrumentsForBaseAssetAndExchangeResponse {
+        instruments: meta_instruments
+            .into_iter()
+            .map(|meta_instrument: MetaInstrument| {
+                meta_instrument_to_proto_instrument(&meta_instrument)
+            })
+            .collect(),
+    }
+}
+
+pub fn get_all_instruments_for_quote_asset_and_exchange_response(
+    meta_instruments: Vec<MetaInstrument>,
+) -> proto_mddb::proto::GetAllInstrumentsForQuoteAssetAndExchangeResponse {
+    proto_mddb::proto::GetAllInstrumentsForQuoteAssetAndExchangeResponse {
+        instruments: meta_instruments
+            .into_iter()
+            .map(|meta_instrument: MetaInstrument| {
+                meta_instrument_to_proto_instrument(&meta_instrument)
+            })
+            .collect(),
+    }
+}
+
+pub fn get_all_instruments_for_base_quote_asset_and_exchange_response(
+    meta_instruments: Vec<MetaInstrument>,
+) -> proto_mddb::proto::GetAllInstrumentsForBaseQuoteAssetAndExchangeResponse {
+    proto_mddb::proto::GetAllInstrumentsForBaseQuoteAssetAndExchangeResponse {
+        instruments: meta_instruments
+            .into_iter()
+            .map(|meta_instrument: MetaInstrument| {
+                meta_instrument_to_proto_instrument(&meta_instrument)
+            })
+            .collect(),
+    }
+}
+
+pub fn get_lookup_instrument_exchange_pair_code_response(
+    instrument_exchange_pair_code: &str,
+) -> proto_mddb::proto::LookupInstrumentExchangePairCodeNameResponse {
+    proto_mddb::proto::LookupInstrumentExchangePairCodeNameResponse {
+        instrument_exchange_pair_code: instrument_exchange_pair_code.to_string(),
+    }
+}
+
+pub fn get_lookup_instrument_figi_response(
+    instrument_figi: Option<String>,
+    instrument_pair_figi: Option<String>,
+) -> proto_mddb::proto::LookupInstrumentFigiResponse {
+    proto_mddb::proto::LookupInstrumentFigiResponse {
+        instrument_pair_figi,
+        instrument_figi,
+    }
+}
+
+// Conversion utils
+pub fn meta_instrument_to_proto_instrument(
+    meta_instrument: &MetaInstrument,
+) -> proto_mddb::proto::ProtoMetaInstrument {
+    proto_mddb::proto::ProtoMetaInstrument {
+        instrument_id: meta_instrument.primary_key(),
+        instrument_code: meta_instrument.code.to_string(),
+        instrument_hash: meta_instrument.hash(),
+        instrument_class: meta_instrument.class.to_string(),
+        instrument_base_asset: meta_instrument.base_asset.to_string(),
+        instrument_quote_asset: meta_instrument.quote_asset.to_string(),
+        instrument_exchanges_code: meta_instrument.exchange_code.to_string(),
+        instrument_exchange_pair_code: meta_instrument.exchange_pair_code.to_string(),
+        instrument_pair_figi: if meta_instrument.metadata.is_some() {
+            Some(
+                meta_instrument
+                    .clone()
+                    .metadata
+                    .unwrap()
+                    .pair_figi
+                    .unwrap_or_default(),
+            )
+        } else {
+            None
+        },
+        instrument_figi: if meta_instrument.metadata.is_some() {
+            Some(
+                meta_instrument
+                    .clone()
+                    .metadata
+                    .unwrap()
+                    .instrument_figi
+                    .unwrap_or_default(),
+            )
+        } else {
+            None
+        },
+        instrument_trade_start_timestamp: meta_instrument.trade_start_timestamp.map(|ts| ts as i64),
+        instrument_trade_end_timestamp: meta_instrument.trade_end_timestamp,
+    }
+}
+
+pub fn proto_instrument_to_meta_instrument(
+    proto_instrument: &proto_mddb::proto::ProtoMetaInstrument,
+) -> MetaInstrument {
+    let metadata = if proto_instrument.instrument_figi.is_some()
+        || proto_instrument.instrument_pair_figi.is_some()
+    {
+        Some(InstrumentMetadata {
+            pair_figi: proto_instrument.instrument_pair_figi.clone(),
+            instrument_figi: proto_instrument.instrument_figi.clone(),
+        })
+    } else {
+        None
+    };
+
+    MetaInstrument {
+        kaiko_legacy_exchange_slug: String::new(), //  kaiko_legacy_exchange_slug is not used
+        trade_start_time: Some(
+            proto_instrument
+                .instrument_trade_start_timestamp
+                .unwrap()
+                .to_string(),
+        ),
+        trade_end_time: Some(
+            proto_instrument
+                .instrument_trade_start_timestamp
+                .unwrap()
+                .to_string(),
+        ),
+        exchange_code: proto_instrument.instrument_exchanges_code.clone(),
+        exchange_pair_code: proto_instrument.instrument_exchange_pair_code.clone(),
+        base_asset: proto_instrument.instrument_base_asset.clone(),
+        quote_asset: proto_instrument.instrument_quote_asset.clone(),
+        kaiko_legacy_symbol: String::new(), //  kaiko_legacy_symbol is not used
+        code: proto_instrument.instrument_code.clone(),
+        class: proto_instrument.instrument_class.clone(),
+        metadata,
+        trade_start_timestamp: proto_instrument
+            .instrument_trade_start_timestamp
+            .map(|ts| ts as u64),
+        trade_end_timestamp: proto_instrument.instrument_trade_end_timestamp,
+        trade_compressed_size: 0, //  trade_compressed_size is not used
+        trade_count: 0,           //  trade_count is not used
+    }
+}
--- queng_proto/proto_mddb_utils/tests/mddb_instruments_utils_tests.rs
+++ queng_proto/proto_mddb_utils/tests/mddb_instruments_utils_tests.rs
@@ -1,1 +1,152 @@
+use common_metadata::prelude::{InstrumentMetadata, MetaInstrument};
+use proto_mddb::proto::*;
+use proto_mddb_utils::mddb_instruments_utils::*;
 
+fn create_test_meta_instrument() -> MetaInstrument {
+    MetaInstrument {
+        exchange_code: "XKRX".to_string(),
+        exchange_pair_code: "BTCUSD".to_string(),
+        base_asset: "BTC".to_string(),
+        quote_asset: "USD".to_string(),
+        code: "BTC-USD".to_string(),
+        class: "currency".to_string(),
+        metadata: Some(InstrumentMetadata {
+            pair_figi: Some("BBG000BLNNH6".to_string()),
+            instrument_figi: Some("BBG000BLNNH7".to_string()),
+        }),
+        trade_start_timestamp: Some(1609459200),
+        trade_end_timestamp: Some(1640995199),
+        trade_compressed_size: 1024,
+        trade_count: 10000,
+        kaiko_legacy_exchange_slug: "".to_string(),
+        trade_start_time: None,
+        trade_end_time: None,
+        kaiko_legacy_symbol: "".to_string(),
+    }
+}
+
+#[test]
+fn test_get_count_instruments_request() {
+    let request = get_count_instruments_request();
+    assert!(matches!(request, CountInstrumentsRequest {}));
+}
+
+#[test]
+fn test_get_check_if_instrument_exists_request() {
+    let instrument_id = "BTC-USD-XKRX";
+    let request = get_check_if_instrument_exists_request(instrument_id);
+    assert_eq!(request.instrument_id, instrument_id);
+}
+
+#[test]
+fn test_get_instrument_by_id_request() {
+    let instrument_id = "BTC-USD-XKRX";
+    let request = get_instrument_by_id_request(instrument_id);
+    assert_eq!(request.instrument_id, instrument_id);
+}
+
+#[test]
+fn test_get_instrument_by_figi_request() {
+    let figi = "BBG000BLNNH6";
+    let request = get_instrument_by_figi_request(figi);
+    assert_eq!(request.instrument_figi, figi);
+}
+
+#[test]
+fn test_get_all_instruments_request() {
+    let request = get_all_instruments_request();
+    assert!(matches!(request, GetAllInstrumentsRequest {}));
+}
+
+#[test]
+fn test_get_all_instruments_for_base_asset_request() {
+    let base_asset = "BTC";
+    let request = get_all_instruments_for_base_asset_request(base_asset);
+    assert_eq!(request.base_asset, base_asset);
+}
+
+#[test]
+fn test_get_count_instruments_response() {
+    let count = 42;
+    let response = get_count_instruments_response(count);
+    assert_eq!(response.count, count);
+}
+
+#[test]
+fn test_get_check_if_instrument_exists_response() {
+    let instrument_id = "BTC-USD-XKRX";
+    let exists = true;
+    let response = get_check_if_instrument_exists_response(instrument_id, exists);
+    assert_eq!(response.instrument_id, instrument_id);
+    assert_eq!(response.exists, exists);
+}
+
+#[test]
+fn test_get_instrument_by_id_response() {
+    let meta_instrument = create_test_meta_instrument();
+    let response = get_instrument_by_id_response(meta_instrument.clone());
+    assert!(response.instrument.is_some());
+    let proto_instrument = response.instrument.unwrap();
+    assert_eq!(
+        proto_instrument.instrument_base_asset,
+        meta_instrument.base_asset
+    );
+    assert_eq!(
+        proto_instrument.instrument_quote_asset,
+        meta_instrument.quote_asset
+    );
+}
+
+#[test]
+fn test_get_all_instruments_response() {
+    let meta_instruments = vec![create_test_meta_instrument()];
+    let response = get_all_instruments_response(meta_instruments);
+    assert_eq!(response.instruments.len(), 1);
+    let proto_instrument = &response.instruments[0];
+    assert_eq!(proto_instrument.instrument_exchanges_code, "XKRX");
+    assert_eq!(proto_instrument.instrument_base_asset, "BTC");
+}
+
+#[test]
+fn test_meta_instrument_to_proto_instrument() {
+    let meta_instrument = create_test_meta_instrument();
+    let proto_instrument = meta_instrument_to_proto_instrument(&meta_instrument);
+
+    assert_eq!(
+        proto_instrument.instrument_base_asset,
+        meta_instrument.base_asset
+    );
+    assert_eq!(
+        proto_instrument.instrument_quote_asset,
+        meta_instrument.quote_asset
+    );
+    assert_eq!(
+        proto_instrument.instrument_exchanges_code,
+        meta_instrument.exchange_code
+    );
+    assert_eq!(proto_instrument.instrument_class, meta_instrument.class);
+    assert_eq!(
+        proto_instrument.instrument_pair_figi,
+        Some("BBG000BLNNH6".to_string())
+    );
+    assert_eq!(
+        proto_instrument.instrument_figi,
+        Some("BBG000BLNNH7".to_string())
+    );
+}
+
+#[test]
+fn test_proto_instrument_to_meta_instrument() {
+    let original = create_test_meta_instrument();
+    let proto = meta_instrument_to_proto_instrument(&original);
+    let converted = proto_instrument_to_meta_instrument(&proto);
+
+    assert_eq!(converted.base_asset, original.base_asset);
+    assert_eq!(converted.quote_asset, original.quote_asset);
+    assert_eq!(converted.exchange_code, original.exchange_code);
+    assert_eq!(converted.class, original.class);
+    assert_eq!(
+        converted.metadata.as_ref().unwrap().pair_figi,
+        original.metadata.as_ref().unwrap().pair_figi
+    );
+}
