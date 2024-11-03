e7953e0549b2067bdb044756bdda57e09444186f
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-03 21:56:20 +0800

    Implemented pg_import_config
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ queng_import/postgres/pg_import_config/src/main.rs
@@ -0,0 +1,40 @@
+mod print_utils;
+
+use config_import::ConfigImportManager;
+use mimalloc::MiMalloc;
+use std::error::Error;
+use std::process::exit;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+const DBG: bool = true;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    print_utils::print_start_header();
+
+    let config_import_manager = ConfigImportManager::with_test_and_debug().await;
+
+    let already_imported = config_import_manager
+        .check_if_portfolios_already_imported()
+        .await;
+
+    // If all services have already been imported, exit the program
+    if already_imported {
+        print_utils::print_already_header();
+        exit(0);
+    }
+
+    // If nothing has been imported yet, import all configs
+    print_utils::dbg_print("Import configs");
+    config_import_manager
+        .import_portfolio_configs()
+        .await
+        .expect("Failed to import configs");
+
+    let nr_of_configs = config_import_manager.count_db_portfolios().await;
+    print_utils::print_stop_header(nr_of_configs, true);
+
+    Ok(())
+}
---
+++ queng_import/postgres/pg_import_config/src/print_utils.rs
@@ -0,0 +1,34 @@
+use crate::DBG;
+
+pub(crate) fn print_start_header() {
+    println!();
+    println!("==========================================");
+    println!("||  Migrating PostgresSQL Database ||",);
+    println!("==========================================");
+    println!();
+}
+
+pub(crate) fn print_already_header() {
+    println!();
+    println!("==========================================");
+    println!("|| Database Already Migrated ||");
+    println!("==========================================");
+    println!();
+}
+
+pub(crate) fn print_stop_header(nr_of_configs: usize, schema_ok: bool) {
+    println!();
+    println!("||  Migrating PostgresSQL Database  ||");
+    println!("==========================================");
+    println!("Migrated PostgresSQL DB schemas: {}", schema_ok);
+    println!("Imported Configs in CMDB schema: {}", nr_of_configs);
+    println!("Migration and Data Import Complete");
+    println!("==========================================");
+    println!();
+}
+
+pub(crate) fn dbg_print(msg: &str) {
+    if DBG {
+        println!("[main]: {}", msg)
+    }
+}
