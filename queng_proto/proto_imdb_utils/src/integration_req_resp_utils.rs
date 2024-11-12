f2e39a83eb9d051c18d89732b2e2ef5744fa03d7
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-12 15:07:52 +0800

    Updated proto_imdb_utils
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_proto/proto_imdb_utils/src/integration_req_resp_utils.rs
+++ queng_proto/proto_imdb_utils/src/integration_req_resp_utils.rs
@@ -1,6 +1,7 @@
 // Request
 
 use crate::integration_utils::integration_config_to_proto;
+use common_exchange::prelude::ExchangeID;
 use common_ims::prelude::IntegrationConfig;
 use proto_imdb::proto::*;
 
@@ -22,10 +23,10 @@ pub fn get_check_if_integration_config_exists_request(
     CheckIfIntegrationConfigExistsRequest { integration_id }
 }
 
-pub fn get_delete_integration_request(integration_id: &str) -> DeleteIntegrationRequest {
-    DeleteIntegrationRequest {
-        integration_id: integration_id.to_string(),
-    }
+pub fn get_check_if_integration_config_online_request(
+    integration_id: String,
+) -> CheckIfIntegrationConfigOnlineRequest {
+    CheckIfIntegrationConfigOnlineRequest { integration_id }
 }
 
 pub fn get_integration_request(integration_id: &str) -> GetIntegrationConfigRequest {
@@ -38,6 +39,34 @@ pub fn get_all_integrations_request() -> GetAllIntegrationsRequest {
     GetAllIntegrationsRequest {}
 }
 
+pub fn get_all_integrations_by_exchange_request(
+    exchange_id: ExchangeID,
+) -> GetAllIntegrationsByExchangeRequest {
+    GetAllIntegrationsByExchangeRequest {
+        exchange_id: exchange_id as i32,
+    }
+}
+
+pub fn get_all_online_integrations_request() -> GetAllOnlineIntegrationsRequest {
+    GetAllOnlineIntegrationsRequest {}
+}
+
+pub fn get_all_offline_integrations_request() -> GetAllOfflineIntegrationsRequest {
+    GetAllOfflineIntegrationsRequest {}
+}
+
+pub fn get_set_integration_online_request(integration_id: &str) -> SetIntegrationOnlineRequest {
+    SetIntegrationOnlineRequest {
+        integration_id: integration_id.to_string(),
+    }
+}
+
+pub fn get_set_integration_offline_request(integration_id: &str) -> SetIntegrationOfflineRequest {
+    SetIntegrationOfflineRequest {
+        integration_id: integration_id.to_string(),
+    }
+}
+
 pub fn get_update_integration_request(
     integration_config: IntegrationConfig,
 ) -> UpdateIntegrationRequest {
@@ -47,4 +76,122 @@ pub fn get_update_integration_request(
     }
 }
 
+pub fn get_delete_integration_request(integration_id: &str) -> DeleteIntegrationRequest {
+    DeleteIntegrationRequest {
+        integration_id: integration_id.to_string(),
+    }
+}
+
 // Response
+
+pub fn get_create_integration_response() -> CreateIntegrationResponse {
+    CreateIntegrationResponse {
+        ok: false,
+        error: None,
+    }
+}
+
+pub fn get_count_integration_response(count: u64) -> CountIntegrationResponse {
+    CountIntegrationResponse { count }
+}
+
+pub fn get_check_if_integration_config_exists_response(
+    exists: bool,
+) -> CheckIfIntegrationConfigExistsResponse {
+    CheckIfIntegrationConfigExistsResponse { exists }
+}
+
+pub fn get_check_if_integration_config_online_response(
+    online: bool,
+) -> CheckIfIntegrationConfigOnlineResponse {
+    CheckIfIntegrationConfigOnlineResponse { online }
+}
+
+pub fn get_integration_config_response(
+    integration_config: Option<IntegrationConfig>,
+) -> GetIntegrationConfigResponse {
+    GetIntegrationConfigResponse {
+        integration: integration_config.map(integration_config_to_proto),
+    }
+}
+
+pub fn get_all_integrations_response(
+    integration_configs: Vec<IntegrationConfig>,
+) -> GetAllIntegrationsResponse {
+    GetAllIntegrationsResponse {
+        integrations: integration_configs
+            .into_iter()
+            .map(integration_config_to_proto)
+            .collect(),
+    }
+}
+
+pub fn get_all_integrations_by_exchange_response(
+    integration_configs: Vec<IntegrationConfig>,
+) -> GetAllIntegrationsResponse {
+    GetAllIntegrationsResponse {
+        integrations: integration_configs
+            .into_iter()
+            .map(integration_config_to_proto)
+            .collect(),
+    }
+}
+
+pub fn get_all_online_integrations_response(
+    integration_configs: Vec<IntegrationConfig>,
+) -> GetAllOnlineIntegrationsResponse {
+    GetAllOnlineIntegrationsResponse {
+        integrations: integration_configs
+            .into_iter()
+            .map(integration_config_to_proto)
+            .collect(),
+    }
+}
+
+pub fn get_all_offline_integrations_response(
+    integration_configs: Vec<IntegrationConfig>,
+) -> GetAllOfflineIntegrationsResponse {
+    GetAllOfflineIntegrationsResponse {
+        integrations: integration_configs
+            .into_iter()
+            .map(integration_config_to_proto)
+            .collect(),
+    }
+}
+
+pub fn get_set_integration_online_response(
+    ok: bool,
+    error: Option<String>,
+) -> SetIntegrationOnlineResponse {
+    SetIntegrationOnlineResponse { ok, error }
+}
+pub fn get_set_integration_offline_response(
+    ok: bool,
+    error: Option<String>,
+) -> SetIntegrationOfflineResponse {
+    SetIntegrationOfflineResponse { ok, error }
+}
+
+pub fn get_update_integration_response(
+    ok: bool,
+    nr_updated: u32,
+    error: Option<String>,
+) -> UpdateIntegrationResponse {
+    UpdateIntegrationResponse {
+        ok,
+        nr_updated,
+        error,
+    }
+}
+
+pub fn get_delete_integration_response(
+    ok: bool,
+    nr_deleted: u32,
+    error: Option<String>,
+) -> DeleteIntegrationResponse {
+    DeleteIntegrationResponse {
+        ok,
+        nr_deleted,
+        error,
+    }
+}
