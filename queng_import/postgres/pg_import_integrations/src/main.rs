8fdbe854b4e6944b3c6df7ba7581e05ff33596c8
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-13 13:27:02 +0800

    Added pg_import_integrations crate
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ queng_import/postgres/pg_import_integrations/src/main.rs
@@ -0,0 +1,12 @@
+use mimalloc::MiMalloc;
+use std::error::Error;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+const DBG: bool = true;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
