0298d35c69e59ee8166da7e757098f76bf21b806
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-15 11:13:10 +0800

    Disabled iggy dependency for the time being due to openssl cross compilation issue.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_message_bus/message_consumer/src/lib.rs
+++ queng_message_bus/message_consumer/src/lib.rs
@@ -1,12 +1,1 @@
-use iggy::client_provider::ClientProviderConfig;
 
-//
-pub struct MessageConsumer {
-    client_config: ClientProviderConfig,
-}
-
-impl MessageConsumer {
-    pub fn client_config(&self) -> &ClientProviderConfig {
-        &self.client_config
-    }
-}
--- queng_message_bus/message_producer/src/lib.rs
+++ queng_message_bus/message_producer/src/lib.rs
@@ -1,12 +1,1 @@
-use iggy::client_provider::ClientProviderConfig;
 
-//
-pub struct MessageProducer {
-    client_config: ClientProviderConfig,
-}
-
-impl MessageProducer {
-    pub fn client_config(&self) -> &ClientProviderConfig {
-        &self.client_config
-    }
-}
