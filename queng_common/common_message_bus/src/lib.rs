bedec94d6cbbef09254192e378fa38ecfbf15806
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-17 11:53:53 +0800

    Updated vendors deps. Updated  queng_message_bus

--- queng_common/common_message_bus/src/lib.rs
+++ queng_common/common_message_bus/src/lib.rs
@@ -1,3 +1,4 @@
+mod ims_data_config;
 mod message_client_channel;
 mod message_client_config;
 pub mod prelude;
--- queng_common/common_message_bus/src/prelude.rs
+++ queng_common/common_message_bus/src/prelude.rs
@@ -1,2 +1,3 @@
+pub use crate::ims_data_config::*;
 pub use crate::message_client_channel::*;
 pub use crate::message_client_config::*;
