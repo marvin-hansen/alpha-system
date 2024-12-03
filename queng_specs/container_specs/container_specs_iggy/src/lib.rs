2de98f958f4aa8bf4246ed69634500ed427e90bf
Marvin Hansen <marvin.hansen@gmail.com>
2024-12-03 14:32:19 +0800

    Updated iggy container specs with env var to disable cache on CI.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_specs/container_specs/container_specs_iggy/src/lib.rs
+++ queng_specs/container_specs/container_specs_iggy/src/lib.rs
@@ -2,15 +2,15 @@ use common_container::{ContainerConfig, WaitStrategy};
 
 pub fn iggy_container_config() -> ContainerConfig<'static> {
     ContainerConfig::new(
-        "ci_iggy",
-        "ghcr.io/marvin-hansen/ci_iggy/ci_iggy",
+        "iggy",
+        "iggyrs/iggy",
         //  When you update the Dockertag,
         // also update the iggy.sh script in scripts/ folder
-        "latest",
+        "0.4.84",
         "0.0.0.0",
         3000,
         Some(&[8090]),
-        None,
+        Some(&["IGGY_SYSTEM_CACHE_ENABLED=false"]), // Have to disable system cache for CI and testing
         None,
         true, // Keep the container running for re-use
         true, // Keep the same container config across all env. setups.
