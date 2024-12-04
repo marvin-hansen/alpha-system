bbd90d96a0c457981f5e81bdd675616f2687144b
Marvin Hansen <marvin.hansen@gmail.com>
2024-12-04 16:08:30 +0800

    Working on data integration trait.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_integration/traits/trait_data_integration/src/lib.rs
+++ queng_integration/traits/trait_data_integration/src/lib.rs
@@ -1,1 +1,23 @@
-pub trait ImsDataIntegration {}
+use enum_dispatch::enum_dispatch;
+use std::fmt::Error;
+
+pub trait TradeProcessor {
+    /// Callback to process a bar of trade data
+    fn process_trade_bar(
+        &self,
+        trades: Vec<u8>,
+    ) -> impl std::future::Future<Output = Result<(), Error>> + Send;
+}
+
+#[enum_dispatch(ImsDataIntegration)]
+pub trait ImsDataIntegration {
+    fn start_date(
+        &self,
+        data_id: &[String],
+    ) -> impl std::future::Future<Output = Result<(), Error>> + Send;
+    fn stop_date(
+        &self,
+        data_id: &[String],
+    ) -> impl std::future::Future<Output = Result<(), Error>> + Send;
+    fn stop_all_date(&self) -> impl std::future::Future<Output = Result<(), Error>> + Send;
+}
