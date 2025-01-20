3f2966f97d9ffa190fc1ede2c4e8a978dde300ce
Marvin Hansen <marvin.hansen@gmail.com>
2025-01-20 12:06:02 +0800

    Added or updated Bazel dependencies

--- thirdparty/crates/docker_utils-0.2.1/tests/mod.rs
+++
@@ -1,7 +0,0 @@
-/*
- * SPDX-License-Identifier: MIT
- * Copyright (c) "2025" . The buildutils Authors and Contributors. All Rights Reserved.
- */
-
-mod container_config_tests;
-mod docker_util_tests;
