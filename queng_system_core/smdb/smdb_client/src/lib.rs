650e9a0e25ddd664689c69644c8bec708ceda959
Marvin Hansen <marvin.hansen@gmail.com>
2025-01-21 12:03:13 +0800

    Improved error message on SMDBClient
     Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>
    
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_system_core/smdb/smdb_client/src/lib.rs
+++ queng_system_core/smdb/smdb_client/src/lib.rs
@@ -19,13 +19,13 @@ impl SMDBClient {
         let s = format!("http://{host}:{smdb_port}");
         let uri = s
             .parse::<Uri>()
-            .unwrap_or_else(|_| panic!("[SMDBProvider]: Failed to parse server URI: {s}"));
+            .unwrap_or_else(|_| panic!("\r\n ❌[SMDBClient]: Failed to parse server URI: {s}"));
 
         // creating a channel that connects to server
         let channel = Channel::builder(uri)
             .connect()
             .await
-            .unwrap_or_else(|_| panic!("\r\n [SMDBProvider]: Failed to connect to SMDB service on: {s} \r\n  \r\n Detail: \r\n"));
+            .unwrap_or_else(|_| panic!("\r\n ❌[SMDBClient]: Failed to connect to SMDB service on: {s} \r\n  \r\n Detail: \r\n"));
 
         let client = SmdbServiceClient::new(channel);
 
