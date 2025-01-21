2ab3e73f287844a044aa133ddbaf2ad2a3279680
Marvin Hansen <marvin.hansen@gmail.com>
2025-01-21 20:02:00 +0800

    Added preliminary test for binance spot IMS data integration.
     Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>
    
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ queng_system_ims_data/binance/binance_spot_tests/tests/binancen_spot_tests.rs
@@ -0,0 +1,59 @@
+use config_manager::CfgManager;
+use service_utils::{ServiceStartConfig, ServiceUtil, WaitStrategy};
+
+fn get_service_wait_strategy(uri: String) -> WaitStrategy {
+    let url = format!("http://{uri}/health");
+    WaitStrategy::WaitForHttpHealthCheck(url, 10)
+}
+
+fn get_service_start_config(program: &'static str, uri: String) -> ServiceStartConfig {
+    ServiceStartConfig::builder()
+        .program(program)
+        .wait_strategy(get_service_wait_strategy(uri))
+        .build()
+}
+
+const ROOT_PATH: &str = "queng_system_ims_data/binance/binance_spot_tests/tests";
+const PROGRAM: &str = "ims_data_service";
+const BINARIES: [&str; 1] = [PROGRAM];
+
+#[tokio::test]
+async fn test_binance_spot() {
+    dbg!("Start service util");
+    let res = ServiceUtil::with_debug(ROOT_PATH, Vec::from(BINARIES)).await;
+    if res.is_err() {
+        dbg!(&res);
+    }
+    assert!(res.is_ok());
+    let svc_util = res.unwrap();
+    dbg!("✅ service util started");
+
+    dbg!("Start config manager");
+    let config_manager = CfgManager::default_with_debug();
+    dbg!("✅ config manager started");
+
+    dbg!("Detect Environment");
+    let env = config_manager.env_type();
+    dbg!(&format!("✅ Detected Environment: {}", env));
+
+    // Start Iggy messaging service
+    // dbg!("Start iggy messaging service");
+
+    dbg!("Configure IMS Data service - Binance Spot");
+    let (uri, _) = config_manager
+        .get_metrics_socket_addr_uri()
+        .expect("Failed to get host and port for IMS Data service");
+
+    dbg!(&format!(" IMS Data service uri: {uri}"));
+
+    dbg!("Configure IMS Data service - Binance Spot");
+    let dbgw_start_config = get_service_start_config(PROGRAM, uri);
+
+    dbg!("Start IMS Data service - Binance Spot");
+    let result = svc_util.start_service_from_config(dbgw_start_config).await;
+    if result.is_err() {
+        dbg!(&result);
+    }
+    assert!(result.is_ok());
+    dbg!("✅ IMS Data service service started");
+}
